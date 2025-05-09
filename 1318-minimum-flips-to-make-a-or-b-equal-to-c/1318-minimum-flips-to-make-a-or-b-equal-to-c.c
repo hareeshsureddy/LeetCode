6087c64983f47cf2b804cd403f3cb6f825b1447b
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-09 23:08:22 +0530

    0 ms (100%) 8.2 MB (8.11%) 1318-minimum-flips-to-make-a-or-b-equal-to-c 05-09-2025 Medium .c

--- 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.c
+++ 1318-minimum-flips-to-make-a-or-b-equal-to-c/1318-minimum-flips-to-make-a-or-b-equal-to-c.c
@@ -2,22 +2,18 @@
 
 int minFlips(int a, int b, int c){
     int result=0;
-    int max=(a>b)?((a>c)?a:c):((b>c)?b:c);
-    int bitCount=(int)floor(log2(max))+1;
+    int bitCount=(int)floor(log2((a>b)?((a>c)?a:c):((b>c)?b:c)))+1;
     for(int i=0;i<bitCount;i++){
         if((c>>i)&1){
             if(!(((a>>i)&1) ||((b>>i)&1))){
-               // a|=(1<<i);
                 result++;
             }
         }
         else{
             if((a>>i)&1){
-                //a&=~(1<<i);
                 result++;
             }
             if((b>>i)&1){
-               //b&=~(1<<i);
                 result++;
             }
         }
