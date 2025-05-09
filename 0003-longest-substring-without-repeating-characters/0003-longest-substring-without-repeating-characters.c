eae356c37892413c116bb53289c3cc6b26a91d8f
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-09 19:35:42 +0530

    15 ms (23.97%) 9.8 MB (65.29%) 0003-longest-substring-without-repeating-characters 05-09-2025 Medium .c

---
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.c
@@ -0,0 +1,17 @@
+int lengthOfLongestSubstring(char* s) {
+    int j=0,i=0,max=0;
+    int map[128]={0};
+    while(s[j]){
+        if(map[s[j]]){
+            int k=j-i;
+            max=(max<k)?k:max;
+            j=i=map[s[j]];
+            memset(map,0,128*sizeof(int));
+        }
+        map[s[j]]=i+1;
+        j++;
+    }
+            int k=j-i;
+            max=(max<k)?k:max;
+            return max;
+}
