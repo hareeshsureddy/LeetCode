d27e7ffa75025eb8d846dc2d32f2d26dafc45ac2
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-13 23:28:27 +0530

    96 ms (62.5%) 16.4 MB (75%) 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k 05-13-2025 Medium .c

---
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.c
@@ -0,0 +1,13 @@
+int minOperations(int* nums, int numsSize, int k) {
+   int XoR=0;
+   for (int i=0;i<numsSize;i++){
+        XoR^=nums[i];
+    }
+    XoR^=k;
+    int result=0;
+    while(XoR){
+        XoR&=(XoR-1);
+        result++;
+    }
+    return result;
+}
