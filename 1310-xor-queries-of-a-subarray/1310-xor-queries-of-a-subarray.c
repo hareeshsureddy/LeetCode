32d247b8ed05c142435166233fff6bde617b1f6b
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-14 00:17:19 +0530

    0 ms (100%) 45.2 MB (100%) 1310-xor-queries-of-a-subarray 05-14-2025 Medium .c

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.c
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.c
@@ -1,19 +1,13 @@
 int* xorQueries(int* arr, int arrSize, int** queries, int queriesSize, int* queriesColSize, int* returnSize) {
-
-    // Initialize prefix array to store cumulative XOR values
-    int prefix[arrSize+1];
     int *ptr = (int *)malloc(sizeof(int) * queriesSize);
-
-    // Calculate prefix array: prefix[i] is the XOR of arr[0] to arr[i]
-    prefix[0] = 0;//arr[0];
-    for (int i = 1; i < arrSize+1; i++) {
-        prefix[i] = prefix[i - 1] ^ arr[i-1];
+    for (int i = 1; i < arrSize; i++) {
+        arr[i] = arr[i - 1] ^ arr[i];
     }
 
     for (int i = 0; i < queriesSize; i++) {
-        // Brancheless
-       // ptr[i] = prefix[queries[i][1]] ^ (prefix[(queries[i][0] - 1 > 0) ? queries[i][0] - 1 : 0] * (queries[i][0] != 0));
-                ptr[i] = (prefix[(queries[i][1])+1] ^ (prefix[(queries[i][0])]));
+        int l=queries[i][0];
+        int r=queries[i][1];
+        ptr[i] = (arr[r] ^ ((l==0)?0:(arr[l-1])));
     } 
 
     // Set the return size to be equal to the number of queries
