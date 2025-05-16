17f295ae2292181a609b7d6e5345eb061d28db6f
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-17 00:41:57 +0530

    0 ms (100%) 10.4 MB (72.73%) 1582-special-positions-in-a-binary-matrix 05-17-2025 Easy .c

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.c
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.c
@@ -1,7 +1,7 @@
 int numSpecial(int** mat, int matSize, int* matColSize) {
     int result=0;
     /*Brute force*/
-    for (int i=0;i<matSize;i++){
+  /*  for (int i=0;i<matSize;i++){
         for (int j=0;j<*matColSize;j++){
             if (mat[i][j]){
                 int rsum=0,csum=0;
@@ -16,6 +16,29 @@ int numSpecial(int** mat, int matSize, int* matColSize) {
                 }
                 if (csum>1) break;
                 if (csum==1 && rsum==1) result++;
+            }
+        }
+    } */
+    int sum=0;
+    int row[matSize],col[*matColSize];
+    for (int i=0;i<matSize;i++){
+        for (int j=0;j<*matColSize;j++){
+            sum+=mat[i][j];
+        }
+        row[i]=sum;
+        sum=0;
+    }
+    for (int i=0;i<*matColSize;i++){
+        for (int j=0;j<matSize;j++){
+            sum+=mat[j][i];
+        }
+        col[i]=sum;
+        sum=0;
+    }
+    for (int i=0;i<matSize;i++){
+        for (int j=0;j<*matColSize;j++){
+            if (mat[i][j]){
+                if(row[i]==1 &&col[j]==1) result++;
             }
         }
     }
