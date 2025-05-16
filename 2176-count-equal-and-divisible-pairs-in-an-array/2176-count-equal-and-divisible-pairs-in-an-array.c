f2beebbb294db14a09ca8350da51bab60c1e9d90
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-16 20:18:19 +0530

    0 ms (100%) 8.2 MB (39.74%) 2176-count-equal-and-divisible-pairs-in-an-array 05-16-2025 Easy .c

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.c
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.c
@@ -2,12 +2,8 @@ int countPairs(int* nums, int numsSize, int k) {
     int result=0;
     for (int i=0;i<numsSize-1;i++){
         for (int j=i+1;j<numsSize;j++){
-            if(nums[i]!=nums[j])
-            continue;
-            else{
-                if(i*j%k==0)
+            if(nums[i]==nums[j] &&(i*j%k==0))
                 result++;
-            }
         }
     }
     return result;
