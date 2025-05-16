c01f1fea2d87ed89b5d9ce720b17f75b31f36dbd
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-17 00:27:50 +0530

    0 ms (100%) 10.7 MB (9.09%) 1582-special-positions-in-a-binary-matrix 05-17-2025 Easy .c

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.c
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.c
@@ -1,14 +1,18 @@
 int numSpecial(int** mat, int matSize, int* matColSize) {
     int result=0;
+    /*Brute force*/
     for (int i=0;i<matSize;i++){
         for (int j=0;j<*matColSize;j++){
             if (mat[i][j]){
                 int rsum=0,csum=0;
                 for (int k=0;k<*matColSize;k++){
                     rsum+=mat[i][k];
+                   if (rsum>1) break;
                 }
+                if (rsum>1) break;
                 for (int k=0;k<matSize;k++){
                     csum+=mat[k][j];
+                    if (csum>1) break;
                 }
                 if (csum==1 && rsum==1) result++;
             }
