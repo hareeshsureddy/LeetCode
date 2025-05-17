a306f22116c4e9f15e7088a0af291c2b36b0d432
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-17 08:33:46 +0530

    Easy 15 ms (89.74%) 9.6 MB (26.5%) 05-17-2025

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.c
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.c
@@ -1,6 +1,7 @@
 int maxProductDifference(int* nums, int numsSize){
     int max1=0,max2=0,min1=10001,min2=10001;
-    for (int i=0;i<numsSize;i++){
+    /*Approach-1*/
+ /*   for (int i=0;i<numsSize;i++){
         if(max1<nums[i]){
             max2=max1;
             max1=nums[i];
@@ -13,6 +14,19 @@ int maxProductDifference(int* nums, int numsSize){
             min1=nums[i];
         }
         else if(nums[i]<min2) min2=nums[i]; 
-}
+} */
+/*Approach-2*/
+   for (int i=0;i<numsSize;i++){
+        if(max1<nums[i]){
+            max2=max1;
+            max1=nums[i];
+        }
+        else if(nums[i]>max2) max2=nums[i];      
+        if(min1>nums[i]){
+            min2=min1;
+            min1=nums[i];
+        }
+        else if(nums[i]<min2) min2=nums[i]; 
+} 
     return ((max1*max2)-(min1*min2));
 }
