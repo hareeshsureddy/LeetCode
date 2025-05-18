981b1af0e15fdc3706f60d94c4bbe8ba41f4300c
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-18 09:51:12 +0530

    Easy 0 ms (100%) 8 MB (90.57%) 05-18-2025

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.c
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.c
@@ -2,10 +2,12 @@ int minOperations(char* s) {
     uint16_t len=strlen(s);
     uint16_t result1=0,result2=0;
     for (int i=0;i<len;i++){
-        char expected1=(i&1)?'0':'1';
+        //Method-1
+    /*    char expected1=(i&1)?'0':'1';
         char expected2=(i&1)?'1':'0';
         if(s[i]!=expected1) result1++;
-        if(s[i]!=expected2) result2++;
+        if(s[i]!=expected2) result2++;  */
+        //Method-2
         /*if(i&1){
         if(s[i]!='0')
         result1++;
@@ -20,6 +22,18 @@ int minOperations(char* s) {
          if(s[i]!='0') 
          result2++;  
         } */
+        //Method-3
+        if(i&1){
+        if(s[i]!='1')
+        result1++;
+        else
+        result2++;
+        }else{
+         if(s[i]!='0') 
+         result1++;
+         else
+         result2++;
+        }  
     }
     return (result1<result2)?result1:result2;
 }
