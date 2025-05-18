c8860df494cd23ba3f5c22caa3fe277cc24e08b8
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-18 20:20:01 +0530

    Medium 65 ms (50%) 94.7 MB (0%) 05-18-2025

---
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.c
@@ -0,0 +1,29 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+ int compare(const void *a, const void *b){
+    return *(int*)a-*(int*)b;
+ }
+int** divideArray(int* nums, int numsSize, int k, int* returnSize, int** returnColumnSizes) {
+    qsort(nums,numsSize,sizeof(int),compare);
+    *returnSize=numsSize/3;
+    int index=0;
+    int** result=(int**)malloc(sizeof(int*)*(numsSize/3));
+    *returnColumnSizes=(int*)malloc(*returnSize*sizeof(int));
+    for(int i=0;i<numsSize;i+=3){
+        if((nums[i+2]-nums[i])<=k){
+            (*returnColumnSizes)[index]=3;
+            result[index]=(int*)malloc(3*sizeof(int));
+            result[index][0]=nums[i];
+            result[index][1]=nums[i+1];
+            result[index++][2]=nums[i+2];
+        }
+        else{
+            *returnSize=0;
+            return NULL;
+        }
+    }
+    return result;
+}
