b724c6c3040daae99220c9ea7c91de2b4825845c
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-19 20:24:55 +0530

    Easy 2 ms (50.59%) 24.7 MB (44.5%) 05-19-2025

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.c
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.c
@@ -5,7 +5,8 @@
     return *(int*)a-*(int*)b;
  }
 int* sortedSquares(int* nums, int numsSize, int* returnSize) {
-   int i=0;
+    /*
+    int i=0;
     while(i<numsSize && nums[i]<0){
         nums[i]=abs(nums[i]);
         i++;
@@ -16,4 +17,17 @@ int* sortedSquares(int* nums, int numsSize, int* returnSize) {
     }
     *returnSize=numsSize;
     return nums;
+    */
+    int *result=(int*)malloc(numsSize*sizeof(int));
+    for(int i=0;i<numsSize;i++){
+        nums[i]=pow(abs(nums[i]),2);
+    }
+    int i=0,j=numsSize-1;
+    int index=j;
+    while(i<j){
+        result[index--]=(nums[i]<nums[j])?nums[j--]:nums[i++];
+    }
+    result[index]=nums[i];
+    *returnSize=numsSize;
+    return result;
 }
