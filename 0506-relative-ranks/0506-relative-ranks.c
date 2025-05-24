3ad73225d4d813f1c14692bd2cf575ee922f753a
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-25 00:04:12 +0530

    Easy 1 ms (88.24%) 11.2 MB (90.59%) 05-25-2025

--- 0506-relative-ranks/0506-relative-ranks.c
+++ 0506-relative-ranks/0506-relative-ranks.c
@@ -1,6 +1,44 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+ /*quick sort*/
+int partition(int *org,int *nums,int start,int end){
+    int pivot=nums[start];
+    int pivot1=org[start];
+    int i=start;
+    int j=end;
+    while(i<j){
+        while(nums[i]>=pivot && i<end){
+            i++;
+        }
+        while(nums[j]<pivot && j>start){
+            j--;
+        }
+        if(i<j){
+            int temp=nums[i];
+            int temp1=org[i];
+            nums[i]=nums[j];
+            org[i]=org[j];
+            nums[j]=temp;
+            org[j]=temp1;
+        }
+    }
+            int temp=nums[j];
+            int temp1=org[j];
+            nums[j]=pivot;
+            org[j]=pivot1;
+            nums[start]=temp;
+            org[start]=temp1;
+    return j;
+}
+void quickSort(int *Org,int *nums,int start,int end){
+        if(start<end){
+            int pi=partition(Org,nums,start,end);
+            quickSort(Org,nums,start,pi-1);
+            quickSort(Org,nums,pi+1,end);
+        }
+} 
+/*
 void insertionSort(int *nums,int *map,int len){
     for (int i=0;i<len-1;i++){
         int index=i;
@@ -15,7 +53,7 @@ void insertionSort(int *nums,int *map,int len){
         nums[index]=temp;
         map[index]=temp1;
     }
-}
+} */
 char** findRelativeRanks(int* score, int scoreSize, int* returnSize) {
     char **result=(char**)calloc(scoreSize,sizeof(char**));
     int map[scoreSize];
@@ -24,7 +62,8 @@ char** findRelativeRanks(int* score, int scoreSize, int* returnSize) {
     map[i]=i;
     }
     *returnSize=scoreSize;
-    insertionSort(score,map,scoreSize);
+/*  insertionSort(score,map,scoreSize); */
+    quickSort(map,score,0,scoreSize-1);
     for(int i=0;i<scoreSize;i++){
         switch(i){
             case 0:
