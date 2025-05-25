8e1b92de4876f7d1e41363c26097a020a3b3b220
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-25 13:17:21 +0530

    Easy 2 ms (29.19%) 12.5 MB (31.68%) 05-25-2025

--- 2942-find-words-containing-character/2942-find-words-containing-character.c
+++ 2942-find-words-containing-character/2942-find-words-containing-character.c
@@ -2,9 +2,10 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* findWordsContaining(char** words, int wordsSize, char x, int* returnSize) {
+    *returnSize=0;
     int *result=(int*)calloc(sizeof(int),wordsSize);
     int k=0;
-    *returnSize=0;
+/*
     for (int i=0;i<wordsSize;i++){
         int len=strlen(words[i]);
         int j=0;
@@ -16,6 +17,13 @@ int* findWordsContaining(char** words, int wordsSize, char x, int* returnSize) {
             }
             j++;
         }
+    } 
+*/
+for (int i=0;i<wordsSize;i++){
+    if(strchr(words[i],x)){
+         result[k++]=i;
+        *returnSize+=1;
     }
+}
     return result;
 }
