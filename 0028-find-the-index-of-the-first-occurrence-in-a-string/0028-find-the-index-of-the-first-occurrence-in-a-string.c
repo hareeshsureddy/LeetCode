da6c1ab6416988c1e9753067b9b150e62ba2c7bb
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-05-31 21:18:01 +0530

    Easy 0 ms (100%) 8 MB (70.31%) 05-31-2025

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.c
@@ -1,6 +1,20 @@
 int strStr(char* haystack, char* needle) {
+    int len1=strlen(haystack);
+    int len2=strlen(needle);
+    if (len1<len2) return -1;
+    int i,j;
+    for( i=0;i<=len1-len2;i++){
+        for ( j=0;j<len2;j++){
+            if(haystack[i+j]!=needle[j])
+            break;
+        }
+        if(j==len2) return i;
+    }
+    return -1; 
+    /*
     int result=(int)strstr(haystack,needle);
     if(result)
     return (int)result-(int)haystack;
     else return -1;
+    */
 }
