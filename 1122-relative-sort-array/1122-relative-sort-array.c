69552e08e6ac40aefc90f1772437dde8711f5244
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-01 07:08:22 +0530

    Easy N/A (0%) N/A (0%) 06-01-2025

--- 1122-relative-sort-array/1122-relative-sort-array.c
+++ 1122-relative-sort-array/1122-relative-sort-array.c
@@ -1,16 +1,33 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+ /*
 int compare(const void *a,const void *b){
 	//Assending Order
 	return *(int*)a-*(int*)b;
 	//Desending Order
 	//return *(int*)b-*(int*)a;
-}
+}*/
 int* relativeSortArray(int* arr1, int arr1Size, int* arr2, int arr2Size, int* returnSize) {
     *returnSize=arr1Size;
     int index=0;
     int *result=(int*)malloc(arr1Size*sizeof(int));
+    int map[1001]={0};
+    for (int j=0;j<arr1Size;j++){
+        map[arr1[j]]++;
+    }
+    for (int i=0;i<arr2Size;i++){
+        while(map[arr2[i]]){
+            result[index++]=arr2[i];
+            map[arr2[i]]--;
+        }
+    }
+    for(int i=0;i<1001;i++){
+        if(map[i])
+        result[index++]=i;
+    }
+    return result;
+    /*
     qsort(arr1,arr1Size,sizeof(arr1[0]),compare);
    // return arr1;
     for (int i=0;i<arr2Size;i++){
@@ -31,4 +48,5 @@ int* relativeSortArray(int* arr1, int arr1Size, int* arr2, int arr2Size, int* re
         result[index++]=arr1[i];
     }
     return result;
+    */
 }
