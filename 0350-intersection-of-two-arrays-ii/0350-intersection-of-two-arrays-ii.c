b2278b4c69f831b009322ef48bec077ee0cd069e
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-01 16:47:09 +0530

    Easy 0 ms (100%) 10.1 MB (57.19%) 06-01-2025

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.c
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.c
@@ -1,21 +1,43 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+ int compare(const void *a, const void *b){
+    return *(int*)a-*(int*)b;
+ }
 int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    qsort(nums1,nums1Size,sizeof(nums1[0]),compare);
+    qsort(nums2,nums2Size,sizeof(nums2[0]),compare);
+    int i=0,j=0,index=0;
+    while(i<nums1Size && j<nums2Size){
+        if(nums1[i]==nums2[j]){
+            nums1[index++]=nums1[i];
+            i++;
+            j++;
+        }
+        else{
+        if(i<nums1Size && j<nums2Size && nums1[i]<nums2[j])
+        i++;
+        else
+        j++;
+        }
+    }
+        *returnSize=index;
+    return nums1;
+    /*
     int map[1001]={0};
     int index=0;
     for (int i=0;i<nums1Size;i++){
         map[nums1[i]]++;
     }
     for (int i=0;i<nums2Size;i++){
-        while(map[nums2[i]]){
+        if(map[nums2[i]]){
             nums1[index++]=nums2[i];
             map[nums2[i]]--;
-            break;
         }
     }
     *returnSize=index;
     return nums1;
+    */
     /*
     int index=0;
     for (int i=0;i<nums1Size;i++){
