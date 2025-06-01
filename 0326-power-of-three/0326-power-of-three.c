d159bbb2529e4cc38e6f2e9d81d81feb6429ea77
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-01 17:47:06 +0530

    Easy 6 ms (24.57%) 8.5 MB (56.42%) 06-01-2025

--- 0326-power-of-three/0326-power-of-three.c
+++ 0326-power-of-three/0326-power-of-three.c
@@ -1,10 +1,12 @@
 bool isPowerOfThree(int n) {
     if(n<1) return false;
-    while(n){
+    /* The maximum power of 3 value that
+       integer can hold is 1162261467 ( 3^19 ) .*/
+    return 1162261467 % n == 0;
+    while(n!=1){
         if(n%3!=0) return false;
         else
         n/=3;
-        if(n==1) return true;
     }
     return true;
 }
