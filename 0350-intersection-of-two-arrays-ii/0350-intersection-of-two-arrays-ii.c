0244c031a789cd150dc0c8dde7d6cc563a1df945
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-01 16:28:30 +0530

    Easy N/A (0%) N/A (0%) 06-01-2025

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.c
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.c
@@ -2,6 +2,20 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* returnSize) {
+    int map[1001]={0};
+    int index=0;
+    for (int i=0;i<nums1Size;i++){
+        map[nums1[i]]++;
+    }
+    for (int i=0;i<nums2Size;i++){
+        while(map[nums2[i]]){
+            nums1[index++]=nums2[i];
+            map[nums2[i]]--;
+        }
+    }
+    *returnSize=index;
+    return nums1;
+    /*
     int index=0;
     for (int i=0;i<nums1Size;i++){
         for (int j=0;j<nums2Size;j++){
@@ -14,4 +28,5 @@ int* intersect(int* nums1, int nums1Size, int* nums2, int nums2Size, int* return
     }
     *returnSize=index;
     return nums1;
+    */
 }
