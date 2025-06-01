dc0bdb09ee76293d411bcc789f67fe95d3942a8e
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-01 17:56:05 +0530

    Easy 0 ms (100%) 8.9 MB (15.11%) 06-01-2025

--- 0326-power-of-three/0326-power-of-three.c
+++ 0326-power-of-three/0326-power-of-three.c
@@ -1,8 +1,16 @@
 bool isPowerOfThree(int n) {
     if(n<1) return false;
+    //Method-1
+    // computing power
+    double p = log10(n) / log10(3);
+    // checking to see if power is an integer or not
+    if (p - (int)p == 0) return true;
+    else return false;
+    //Method-2
     /* The maximum power of 3 value that
        integer can hold is 1162261467 ( 3^19 ) .*/
     return 1162261467 % n == 0;
+    //Method-3
     while(n!=1){
         if(n%3!=0) return false;
         else
