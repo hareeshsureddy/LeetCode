bdcab0fa83583167fd7dfe99170bc693c7056338
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-02 23:16:47 +0530

    Easy 2 ms (55%) 9.5 MB (30%) 06-02-2025

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.c
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.c
@@ -1,11 +1,12 @@
 bool canBeEqual(int* target, int targetSize, int* arr, int arrSize) {
-    int map[1001]={0};
+    int map1[1001]={0};
+    int map2[1001]={0};
     for (int i=0;i<targetSize;i++){
-        map[target[i]]++;
-        map[arr[i]]++;
+        map1[target[i]]++;
+        map2[arr[i]]++;
     }
     for (int i=0;i<targetSize;i++){
-         if(map[target[i]]!=2) return false;
+         if(map1[target[i]]!=map2[target[i]]) return false;
     }
     return true;
 }
