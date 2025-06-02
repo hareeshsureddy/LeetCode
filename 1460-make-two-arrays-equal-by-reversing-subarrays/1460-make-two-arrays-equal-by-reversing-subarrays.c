ad5f6579d6af9dc69f9ffd057b2ba072622436d7
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-02 23:29:53 +0530

    Easy 4 ms (35%) 8.7 MB (70%) 06-02-2025

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.c
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.c
@@ -1,4 +1,8 @@
+int compare(const void *a, const void *b){
+    return *(int*)a - *(int*)b;
+}
 bool canBeEqual(int* target, int targetSize, int* arr, int arrSize) {
+    /* //Method -1
     int map1[1001]={0};
     int map2[1001]={0};
     for (int i=0;i<targetSize;i++){
@@ -7,6 +11,13 @@ bool canBeEqual(int* target, int targetSize, int* arr, int arrSize) {
     }
     for (int i=0;i<targetSize;i++){
          if(map1[target[i]]!=map2[target[i]]) return false;
+    }
+    return true;
+    */
+    qsort(target,targetSize,sizeof(target[0]),compare);
+    qsort(arr,targetSize,sizeof(target[0]),compare);
+        for (int i=0;i<targetSize;i++){
+         if(target[i]!=arr[i]) return false;
     }
     return true;
 }
