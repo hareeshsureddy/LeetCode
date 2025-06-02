b9f0b1d4ef31632a307a3c7177859ccfa5c2106e
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-02 22:11:27 +0530

    Easy N/A (0%) N/A (0%) 06-02-2025

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.c
@@ -0,0 +1,10 @@
+int minOperations(char** logs, int logsSize) {
+    int opeCnt=0;
+  for (int i=0;i<logsSize;i++){
+    if(!strcmp(logs[i],"../") && opeCnt )
+    opeCnt--;
+    else if(strcmp(logs[i],"./"))
+    opeCnt++;
+  }  
+  return opeCnt;
+}
