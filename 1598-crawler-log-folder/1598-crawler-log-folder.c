ffdfe3355e85772b33b1759d805ca2d17ddca5d9
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-02 22:11:53 +0530

    Easy 0 ms (100%) 8.8 MB (90%) 06-02-2025

--- 1598-crawler-log-folder/1598-crawler-log-folder.c
+++ 1598-crawler-log-folder/1598-crawler-log-folder.c
@@ -1,8 +1,10 @@
 int minOperations(char** logs, int logsSize) {
     int opeCnt=0;
   for (int i=0;i<logsSize;i++){
-    if(!strcmp(logs[i],"../") && opeCnt )
+    if(!strcmp(logs[i],"../")){
+    if(opeCnt)
     opeCnt--;
+    }
     else if(strcmp(logs[i],"./"))
     opeCnt++;
   }  
