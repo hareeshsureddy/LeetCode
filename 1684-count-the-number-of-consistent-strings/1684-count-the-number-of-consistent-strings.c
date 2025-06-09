e828d4b1c51d0acc5d00163a2648ea43471d21df
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-09 23:27:49 +0530

    Easy 38 ms (62.97%) 14.4 MB (99.61%) 06-09-2025

---
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.c
@@ -0,0 +1,66 @@
+
+#define hareesh     0xFF
+
+int countConsistentStrings(char * allowed, char ** words, int wordsSize){
+    #if (hareesh==0x00)
+        // allowedBits will represent the bitmask of allowed characters
+        int allowedBits = 0;
+
+        // Set the corresponding bit for each character in allowed
+        for (int i = 0;allowed[i]!='\0'; i++) {
+            allowedBits |= 1 << (allowed[i] - 'a');
+        }
+
+        int consistentCount = 0;
+
+        // Iterate through each word in the words array
+       for (int i = 0;i<wordsSize; i++) {
+            bool isConsistent = true;
+
+            // Check each character in the word
+            for (int j = 0; words[i][j]!='\0'; j++) {
+                // Calculate the bit position for the current character
+                int bit = (allowedBits >> (words[i][j] - 'a')) & 1;
+
+                // If the bit is 0, the character is not allowed
+                if (bit == 0) {
+                    isConsistent = false;
+                    break;
+                }
+            }
+        #else
+            // Create a vector to mark which characters are allowed
+        int isAllowed[26]={0};
+    memset(isAllowed,1,sizeof(isAllowed));
+    int len=strlen(allowed);
+        // Mark all characters in 'allowed' as true in the isAllowed vector
+         for (int i = 0;i<len; i++) {
+            isAllowed[allowed[i] - 'a'] = false;
+        }
+
+        int consistentCount = 0;
+
+        // Iterate through each word in the words array
+         for (int i = 0;i<wordsSize; i++) {
+           // bool isConsistent = true;
+
+            // Check each character of the current word
+            for (int j = 0;words[i][j]!='\0'; j++) {
+                // If any character is not allowed, mark as inconsistent and
+                // break
+                if (isAllowed[words[i][j] - 'a']) {
+                    consistentCount++;
+                    //isConsistent = false;
+                    break;
+                }
+            }
+            #endif
+
+            // If the word is consistent, increment the count
+          //  if (isConsistent) {
+        //    consistentCount++;
+          //  }
+        }
+
+        return wordsSize-consistentCount;
+    }
