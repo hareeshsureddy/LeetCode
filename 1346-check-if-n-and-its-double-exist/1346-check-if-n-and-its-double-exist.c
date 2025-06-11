b9c8943bc82d98d55fa4063026bb844b852b7fbf
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-11 23:20:35 +0530

    Easy 0 ms (100%) 9.2 MB (5.51%) 06-11-2025

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.c
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.c
@@ -1,11 +1,16 @@
 bool checkIfExist(int* arr, int arrSize) {
     int map[2001]={0};
+    int zeroCnt=0;
     for(int i=0;i<arrSize;i++){
         if(!(arr[i]&1)){
         if(map[1000+arr[i]])
         return true;
         }
-        if(arr[i]!=0 && -501<arr[i] && arr[i]<501)
+        if(!arr[i]){
+            zeroCnt++;
+            continue;
+        }
+        if(-501<arr[i] && arr[i]<501)
         map[1000+(arr[i]*2)]=1;
     }
     for(int i=0;i<arrSize;i++){
@@ -14,5 +19,6 @@ bool checkIfExist(int* arr, int arrSize) {
         return true;
         }       
     }
+    if(zeroCnt>1)   return true;
     return false;
 }
