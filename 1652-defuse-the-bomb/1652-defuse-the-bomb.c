a81f1f54eb9f645347dc17a928f4281747a8d1b5
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-11 21:06:53 +0530

    Easy 0 ms (100%) 10.3 MB (12.33%) 06-11-2025

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.c
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.c
@@ -16,6 +16,7 @@ int* decrypt(int* code, int codeSize, int k, int* returnSize) {
         return code;
     }
     int *result=(int*)calloc(codeSize,sizeof(int));
+    /*
         for (int i=0;i<codeSize;i++){
             int sum=0;
                 for (int j=1;j<=abs(k);j++){
@@ -24,7 +25,32 @@ int* decrypt(int* code, int codeSize, int k, int* returnSize) {
                     else
                     sum+=code[(i+codeSize-j)%codeSize];
                 }
-        result[i]=sum;;
+        result[i]=sum;
         }
+        */
+        int i,j;
+    if(k>0){
+        for(i=1;i<=k;i++){
+        result[0]+=code[i];
+        }
+        for(i=1;i<codeSize;i++){
+            k++;
+            result[i]=result[i-1]+code[k%codeSize]-code[i];
+        }
+    }else{
+      i=codeSize-abs(k);
+      j=codeSize-1;
+      while(i<=j){
+        result[0]+=code[i++];
+      }
+      j++;
+      i=codeSize-abs(k);
+      for(int l=1;l<codeSize;l++){
+        result[l]=result[l-1]-code[i%codeSize]+code[j%codeSize];
+        i++;
+        j++;
+      }
+    }
     return result;
+    
 }
