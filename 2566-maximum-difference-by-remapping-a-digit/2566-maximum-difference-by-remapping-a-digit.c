51412dc17eda105694380e34f1d985bb9ef2d0d8
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-14 20:58:57 +0530

    Easy N/A (0%) N/A (0%) 06-14-2025

---
+++ 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.c
@@ -0,0 +1,37 @@
+int minMaxDifference(int num) {
+ int arr[9]={-1};
+ int temp=num;
+ int i=0;
+ while(temp){
+    arr[i]=temp%10;
+    temp/=10;
+    i++;
+ }
+ int max=0;
+ int j=i-1;
+ for (;j>=0;j--){
+    if(arr[j]!=9)
+    max=arr[j];
+ }
+ int maxNum=0;
+  for (j=0;j<i;j++){
+    if(arr[j]==max)
+    arr[j]=9;
+    maxNum+=arr[j]*pow(10,j);
+ }
+   temp=num;
+  i=0;
+ while(temp){
+    arr[i]=temp%10;
+    temp/=10;
+    i++;
+ }
+ int min=arr[i-1];
+ int minNum=0;
+   for (j=0;j<i;j++){
+    if(arr[j]==min)
+    arr[j]=0;
+    minNum+=arr[j]*pow(10,j);
+ }
+ return maxNum-minNum;
+}
