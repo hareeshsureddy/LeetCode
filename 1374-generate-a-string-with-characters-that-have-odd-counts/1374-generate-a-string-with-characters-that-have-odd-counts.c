2b7e6dc56f7f61da2d2967c2c1a946cc83dcbbbf
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-14 23:07:31 +0530

    Easy 0 ms (100%) 8.1 MB (47.83%) 06-14-2025

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.c
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.c
@@ -1,12 +1,8 @@
 char* generateTheString(int n) {
     char *result=(char*)malloc((n+1)*sizeof(char));
-    if(n&1){
-        for(int i=0;i<n;i++)
-        result[i]='h';
-    }else{
-        for(int i=0;i<n-1;i++)
-        result[i]='h';
-        result[n-1]='s';
+    memset(result,'h',n*sizeof(char));
+    if(!(n&1)){
+        result[n-1]++;
     }
     result[n]='\0';
     return result;
