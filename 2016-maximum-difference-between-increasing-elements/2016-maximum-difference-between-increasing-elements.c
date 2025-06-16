66e9c6064b96ebf099289b394ce9d06f8aad1643
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-16 22:14:14 +0530

    Easy 0 ms (100%) 8.3 MB (40.96%) 06-16-2025

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.c
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.c
@@ -1,6 +1,7 @@
 int maximumDifference(int* nums, int numsSize) {
-   int minIndex,maxIndex,tryIndex=0;
-   while(minIndex>=maxIndex && tryIndex<=numsSize-2 ){
+    /*
+   int minIndex,maxIndex,tryIndex=0,result=0;
+   while(tryIndex<=numsSize-2 ){
     minIndex=0;
    for(int i=1;i<numsSize-tryIndex;i++){
         minIndex=(nums[minIndex]>nums[i])?i:minIndex;
@@ -10,8 +11,21 @@ int maximumDifference(int* nums, int numsSize) {
         maxIndex=(nums[maxIndex]<nums[i])?i:maxIndex;
    }
    tryIndex++;
+   if(minIndex<maxIndex)
+   result=(nums[maxIndex]-nums[minIndex]>result)?nums[maxIndex]-nums[minIndex]:result;
  }
- if(minIndex<maxIndex)  return nums[maxIndex]-nums[minIndex];
+ if(minIndex<maxIndex)  return result;
  else
  return -1;
+ */
+int min=nums[0],maxDiff=0;
+for (int i=1;i<numsSize;i++){
+    if(min<nums[i])
+    maxDiff=(maxDiff<nums[i]-min)?nums[i]-min:maxDiff;
+    else
+    min=nums[i];
+
+}
+if(maxDiff) return maxDiff;
+else return -1;
 }
