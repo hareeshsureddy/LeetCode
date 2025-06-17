710fed066c90dafb96b2239e3c38f3208cc15842
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-17 22:56:30 +0530

    Easy 10 ms (27.27%) 9.7 MB (27.27%) 06-17-2025

--- 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.c
+++ 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.c
@@ -1,9 +1,9 @@
 int compare(const void *a, const void *b){
     return *(int*)a-*(int*)b;
 }
-
 int minOperations(int* nums, int numsSize, int k) {
     int cnt=0;
+    /*
     qsort(nums,numsSize,sizeof(nums[0]),compare);
     for (int i=numsSize-1;i>0;i--){
         if(nums[i]==nums[i-1]) continue;
@@ -15,4 +15,13 @@ int minOperations(int* nums, int numsSize, int k) {
     if (nums[0]==k) return cnt;
     if(nums[0]>k) return cnt+1;
     return -1;
+    */
+    int map[101]={0};
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]<k) return -1;
+        if(k!=nums[i] && !map[nums[i]])
+        cnt++;
+        map[nums[i]]=1;
+    }
+    return cnt;
 }
