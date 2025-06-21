a5af1eb563f10a8585c191e4c4e535702a2a3266
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-21 15:30:40 +0530

    Medium 4 ms (33.33%) 11.5 MB (33.33%) 06-21-2025

--- 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.c
+++ 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.c
@@ -1,9 +1,28 @@
+int compare(const void*a,const void *b){
+    return *(int*)a-*(int*)b;
+}
+
 int minimumDeletions(char* word, int k) {
     int map[26]={0};
     for (int i=0;word[i];i++){
         map[word[i]-'a']++;
     }
+    qsort(map,26,sizeof(int),compare);
     int minimumDeletions=0x7FFFFFFF;
+    int cumulativeSum=0;
+    for(int i=0;i<26;i++){
+        if(!map[i]) continue;
+        int delete=cumulativeSum;
+        for(int j=25;j>i;j--){
+            if((map[j]-map[i]<=k)) break;
+                    delete+=(map[j]-map[i]-k);
+            }
+         minimumDeletions=fmin(minimumDeletions,delete);
+        cumulativeSum+=map[i];
+        }
+        return minimumDeletions;
+    }
+    /*
     for(int i=0;i<26;i++){
         if(!map[i]) continue;
         int delete=0;
@@ -16,6 +35,6 @@ int minimumDeletions(char* word, int k) {
             }else if(map[i]-map[j]) delete+=map[j];
         }
         minimumDeletions=fmin(minimumDeletions,delete);
-    }
+    }   
     return minimumDeletions;
-}
+} */
