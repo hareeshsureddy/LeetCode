4fd06959229f28997ca129a318e175c2dcaf6801
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-21 09:27:50 +0530

    Medium 3 ms (12.01%) 8.8 MB (67.1%) 06-21-2025

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.c
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.c
@@ -13,7 +13,9 @@ int maxProduct(int* nums, int numsSize) {
     return maxProduct;
     */
     int max=1,min=1,maxProduct=0x80000000;
+    int numsMax=0x80000000;
     for(int i=0;i<numsSize;i++){
+        if(numsMax<nums[i]) numsMax=nums[i];
       if(nums[i]==0){
         min=1;
         max=1;
@@ -24,5 +26,5 @@ int maxProduct(int* nums, int numsSize) {
          min=fmin(min*nums[i],fmin(temp,nums[i]));
          maxProduct=fmax(maxProduct,max);
      }
-     return maxProduct;
+     return fmax(numsMax,maxProduct);
 }
