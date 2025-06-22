c5a31387ae280f262450b2a52464e6729d8da733
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-22 19:11:22 +0530

    Medium 8 ms (39.01%) 10.7 MB (92.92%) 06-22-2025

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.c
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.c
@@ -6,10 +6,18 @@
  * };
  */
 void deleteNode(struct ListNode* node) {
+    //T.C  O(1)
+    node->val=node->next->val;
+   // if(node->next->next==NULL)
+   // node->next=NULL;
+    //else
+    node->next=node->next->next;
+    /* //T.C  O(n)
     while(node->next->next!=NULL){
         node->val=node->next->val;
         node=node->next;
     }
     node->val=node->next->val;
     node->next=NULL;
+    */
 }
