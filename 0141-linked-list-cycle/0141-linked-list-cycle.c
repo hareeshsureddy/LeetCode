b69a5c1b8245d1c656eb42cd8689b8a0317034a1
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-22 18:20:44 +0530

    Easy 9 ms (77.85%) 11.4 MB (18.64%) 06-22-2025

--- 0141-linked-list-cycle/0141-linked-list-cycle.c
+++ 0141-linked-list-cycle/0141-linked-list-cycle.c
@@ -9,7 +9,7 @@ bool hasCycle(struct ListNode *head) {
     if(head){
     struct ListNode *ptr1=head->next;    
     struct ListNode *ptr2=head->next; 
-    while(ptr1!=NULL && ptr2->next!=NULL){
+    while(ptr2!=NULL && ptr2->next!=NULL){
         ptr1=ptr1->next;
         ptr2=ptr2->next->next;
      if(ptr1==ptr2) return true;
