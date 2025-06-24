92b57279d3b5410965a1e5ec9b346f3a08e76090
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-24 06:47:52 +0530

    Easy 41 ms (52.78%) 17.2 MB (65.51%) 06-24-2025

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.c
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.c
@@ -9,6 +9,15 @@ struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *he
         
        struct ListNode *temp1=headA;
        struct ListNode *temp2=headB;
+          while(temp1!=temp2){
+            temp1=temp1->next;
+            temp2=temp2->next;
+            if(temp1==temp2) return temp1;
+            if(temp1==NULL) temp1=headB;
+          if(temp2==NULL)  temp2=headA;
+        }
+        return temp1; 
+/*
         while(temp1!=NULL && temp2!=NULL){
             if(temp1==temp2) return temp1;
             temp1=temp1->next;
@@ -29,6 +38,7 @@ struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *he
             temp2=temp2->next;
         }
         return NULL;
+*/
    /* int len1=0,len2=0;
     struct ListNode *temp=headA;
     // Approach-2
