cbc5aeb89cc0540204cf21253ffb42ef2555b742
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-06-28 08:04:41 +0530

    Easy 3 ms (57.14%) 11.8 MB (28.57%) 06-28-2025

--- 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.c
+++ 2099-find-subsequence-of-length-k-with-the-largest-sum/2099-find-subsequence-of-length-k-with-the-largest-sum.c
@@ -1,6 +1,7 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+ /*
 void bubbleSort_index(int *nums, int numsSize, int *actualIndex){
     for (int i=0;i<numsSize-1;i++){
         for (int j=0;j<numsSize-i-1;j++){
@@ -37,5 +38,41 @@ int* maxSubsequence(int* nums, int numsSize, int k, int* returnSize) {
     for(int i=0;i<k;i++)
     sortNums[i]=nums[actualIndex[i]];
     *returnSize=k;
+    return sortNums;
+} */
+typedef struct{
+    int index;
+    int value;
+}data_t;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ typedef struct{
+    int index;
+    int value;
+}data_t;
+ */
+ int sortValueDec(const void *a, const void *b){
+        return ((data_t*)b)->value - ((data_t*)a)->value;
+ }
+int sortIndexAsc(const void *a, const void *b){
+        return (((data_t*)a)->index-((data_t*)b)->index);
+ }
+int* maxSubsequence(int* nums, int numsSize, int k, int* returnSize) {
+    data_t *values=(data_t*)malloc(sizeof(data_t)*numsSize);
+    for (int i=0;i<numsSize;i++){
+        values[i].index=i;
+        values[i].value=nums[i];
+    }
+    qsort(values,numsSize,sizeof(data_t),sortValueDec);
+    qsort(values,k,sizeof(data_t),sortIndexAsc);
+    int *sortNums=(int*)malloc(sizeof(int)*numsSize);
+    for(int i=0;i<k;i++){
+        sortNums[i]=values[i].value;
+    }
+        *returnSize=k;
     return sortNums;
 }
