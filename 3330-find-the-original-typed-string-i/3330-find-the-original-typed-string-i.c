fba0515018b5e84b1ace73dc3f8c263a55641862
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-07-01 23:33:50 +0530

    Easy 2 ms (42.31%) 8.4 MB (11.54%) 07-01-2025

--- 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.c
+++ 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.c
@@ -1,11 +1,15 @@
 int possibleStringCount(char* word) {
     int stringCnt=0;
     int len=strlen(word);
+    for (int i=1;i<len;i++){
+       if (word[i-1]==word[i]) stringCnt++;
+    }
+    /*
     int i=0,j=0;
     while(i<len-1){
         j=i+1;
         while(word[i]==word[j++]) stringCnt++;
         i=j-1;
-    }
+    } */
     return stringCnt+1;
 }
