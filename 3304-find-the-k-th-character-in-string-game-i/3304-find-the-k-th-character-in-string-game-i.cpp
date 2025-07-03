8cd3ba62e4db3921668d422abbf13da9310999a1
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-07-03 23:42:39 +0530

    Easy 0 ms (100%) 7.7 MB (99.62%) 07-03-2025

---
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    char kthCharacter(int k) {
+    /*
+    char *char_ptr=(char*)malloc(sizeof(char)*k);
+    int i=1;
+    char_ptr[0]='a';
+    while(i<k){
+        int end=i;
+        for (int j=0;j<end;j++){
+            char_ptr[i++]=(char_ptr[j]=='z')?'a':char_ptr[j]+1;
+            if(i==k) break;
+        }
+    }
+    return char_ptr[k-1];
+    */
+    int bitCnt=0;
+    k--;
+    while(k){
+        bitCnt++;
+        k=k&(k-1);
+    }
+    return 'a'+bitCnt;
+    }
+};
