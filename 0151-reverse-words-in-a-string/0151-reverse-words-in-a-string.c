22f32a7def57c86fe59b6315da0c47d334d5d37d
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-07-04 23:12:59 +0530

    Medium 0 ms (100%) 8.5 MB (61.42%) 07-04-2025

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.c
@@ -0,0 +1,17 @@
+char* reverseWords(char* s) {
+    int len=strlen(s);
+    char *result=(char*)malloc((len+1)*sizeof(char));
+    int i,k,j=len-1,idx=0;
+    while(i>=0){
+        while(j>=0 && s[j]==' ') j--;
+        if(j<0) break;
+        i=j;
+        while(i>=0 && s[i]!=' ') i--;
+        for(k=i+1;k<=j;k++)
+        result[idx++]=s[k];
+        result[idx++]=' ';
+        j=i;
+    }
+    result[idx-1]='\0';
+    return result;
+}
