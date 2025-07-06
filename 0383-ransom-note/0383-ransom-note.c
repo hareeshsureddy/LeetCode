ef832eb55814fcb630fb7d40ad4c837673528e92
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-07-06 23:31:06 +0530

    Easy 3 ms (41.08%) 8.9 MB (31.65%) 07-06-2025

--- 0383-ransom-note/0383-ransom-note.c
+++ 0383-ransom-note/0383-ransom-note.c
@@ -1,4 +1,5 @@
 bool canConstruct(char* ransomNote, char* magazine) {
+    /* //Method-1
     int map1[26]={0};
     int map2[26]={0};
     int i=0;
@@ -14,6 +15,19 @@ bool canConstruct(char* ransomNote, char* magazine) {
         if (map1[ransomNote[i]-'a']>map2[ransomNote[i]-'a'])
         return false;
         i++;
+    }
+    return true;
+    */
+    //Method-2
+    int map[26]={0};
+        int i=0;
+        while(magazine[i]){
+        map[magazine[i++]-'a']++;
+    }
+    i=0;
+    while(ransomNote[i]){
+        map[ransomNote[i]-'a']--;
+        if(map[ransomNote[i++]-'a']<0) return false;
     }
     return true;
 }
