c002a90cb30fad73788356ef2c8d1ed28f0ae0a0
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-07-10 23:36:35 +0530

    Easy 3 ms (36.6%) 7.7 MB (25.73%) 07-10-2025

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.c
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.c
@@ -1,6 +1,7 @@
 
 
 char * defangIPaddr(char * address){
+    /*
     char *res=(char*)malloc(22*sizeof(char));
     int i=0,indexx=0;
     while(address[i]){
@@ -11,7 +12,18 @@ char * defangIPaddr(char * address){
         }else
         res[indexx++]=address[i];
         i++;
-    }
+    }*/
+    int len=strlen(address);
+    char *res=(char*)malloc((len+7)*sizeof(char));
+    int i=0,indexx=0;
+    for (;i<len;i++){
+        if(address[i]=='.'){
+        res[indexx++]='[';
+        res[indexx++]='.';
+        res[indexx++]=']';
+        }else
+        res[indexx++]=address[i];
+    }    
     res[indexx]='\0';
     return res;
 }
