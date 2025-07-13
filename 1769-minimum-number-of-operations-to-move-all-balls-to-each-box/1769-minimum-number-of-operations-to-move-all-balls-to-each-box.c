3145122832138e0596980bd79537e229a5649dc5
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-07-13 10:13:10 +0530

    Medium 1 ms (77.08%) 15.8 MB (93.23%) 07-13-2025

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.c
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.c
@@ -5,6 +5,8 @@ int* minOperations(char* boxes, int* returnSize) {
     int len=strlen(boxes);
     *returnSize=len;
     int *res=(int*)calloc(len,sizeof(int));
+    //method -1
+    /*
     for(int i=0;i<len;i++){
         for(int j=0;j<len;j++){
             if(i!=j && boxes[j]=='1'){
@@ -12,5 +14,20 @@ int* minOperations(char* boxes, int* returnSize) {
             }
         }
     }
-    return res;
+    */
+    int cumSum=0;
+    int cumValue=0;
+    for(int i=0;i<len;i++){
+        res[i]+=cumSum;
+        if(boxes[i]=='1') cumValue++;
+        cumSum+=cumValue;
+    }
+    cumSum=0;
+    cumValue=0;
+        for(int i=len-1;i>=0;i--){
+        res[i]+=cumSum;
+        if(boxes[i]=='1') cumValue++;
+        cumSum+=cumValue;
+    }
+        return res;
 }
