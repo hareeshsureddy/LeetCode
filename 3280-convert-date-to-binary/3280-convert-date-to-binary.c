fbe295207e8ccf108db606328fb7c019b9cd065b
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-07-13 11:31:28 +0530

    Easy N/A (0%) N/A (0%) 07-13-2025

---
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.c
@@ -0,0 +1,52 @@
+char* convertDateToBinary(char* date) {
+    char *res=(char*)malloc(22*sizeof(char));
+    int num=0,i=0,j=0;
+        num=(date[0]-'0')*1000;
+        num+=(date[1]-'0')*100;
+        num+=(date[2]-'0')*10;
+        num+=(date[3]-'0');
+        while(num){//year
+            res[i++]=num%2+'0';
+            num/=2;
+        }
+        int len=i;
+        i--;
+        while(j<i){
+            char temp=res[i];
+            res[i--]=res[j];
+            res[j++]=temp;
+        }
+        res[len]='-';
+       j=i=len+1;
+        num=(date[5]-'0')*10;
+        num+=(date[6]-'0');
+        while(num){
+            res[i++]=num%2+'0';
+            num/=2;
+        }
+        len=i;
+        i--;
+        while(j<i){
+            char temp=res[i];
+            res[i--]=res[j];
+            res[j++]=temp;
+        }
+        res[len]='-';
+        j=i=len+1;
+        num=(date[8]-'0')*10;
+        num+=(date[9]-'0');
+        printf("%d",num);
+        while(num){
+            res[i++]=num%2+'0';
+            num/=2;
+        }
+        len=i;
+        i--;
+            while(j<i){
+            char temp=res[i];
+            res[i--]=res[j];
+            res[j++]=temp;
+        }
+        res[len]='\0';
+        return res;
+}
