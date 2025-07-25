f167d1a999b43ea41167b330fa292b467ac73cc8
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-07-25 23:43:29 +0530

    Easy 0 ms (100%) 8.1 MB (34.38%) 07-25-2025

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.c
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.c
@@ -1,5 +1,4 @@
-
-
+/* //Method-1
 char * sortSentence(char * s){
     int len=strlen(s);
     char *res=(char*)malloc(sizeof(char)*len+10);
@@ -17,4 +16,40 @@ char * sortSentence(char * s){
     }
     res[indexx-1]='\0';
     return res;
+} */
+//Method-2
+char * sortSentence(char * s){
+    int wordCnt=1;
+    int i=0;
+    while(s[i]){
+        if(s[i++]==' ') wordCnt++;
+    }
+    //"is2 sentence4 This1 a3"
+    char **wordList=(char**)malloc(sizeof(char*)*wordCnt);
+    char *res=(char*)malloc(sizeof(char)*strlen(s));
+    char *start=s;
+    i=0;
+    while(s[i]){
+        if(s[i]==' '|| s[i+1]=='\0'){
+            if(s[i+1]=='\0'){
+                wordList[s[i]-'0'-1]=start;
+                s[i]='\0';
+                break;
+            }
+            wordList[(s[i-1]-'0')-1]=start;
+            s[i-1]='\0';
+            start=&s[i+1];
+        }
+        i++;
+    }
+    int indexx=0;
+    for(int i=0;i<wordCnt;i++){
+        int j=0;
+        while(wordList[i][j]){
+            res[indexx++]=wordList[i][j++];
+        }
+        res[indexx++]=' ';
+    }
+     res[indexx-1]='\0';
+     return res;
 }
