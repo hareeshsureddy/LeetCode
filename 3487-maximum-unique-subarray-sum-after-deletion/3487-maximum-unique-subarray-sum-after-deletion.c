0dbd293f81d96a85e703bd38b2fd66dff4c48142
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-07-26 00:05:47 +0530

    Easy 0 ms (100%) 9.8 MB (18.75%) 07-26-2025

---
+++ 3487-maximum-unique-subarray-sum-after-deletion/3487-maximum-unique-subarray-sum-after-deletion.c
@@ -0,0 +1,26 @@
+int maxSum(int* nums, int numsSize) {
+    // Index 0 corresponds to -100, index 200 corresponds to 100
+    int freq[201] = {0};    
+    int result = 0, max_num = INT_MIN;
+
+    //count the number of occurrences of each number
+    for(int i = 0; i < numsSize; ++i){
+        freq[nums[i] + 100]++;
+        //finding the maximum number in the array
+        if(nums[i] > max_num) max_num = nums[i];
+    }
+    //now we go through and add up all the
+    //positive numbers whose frequency is greater than 0
+    for(int i = 0; i < 201; ++i){
+        int num = i - 100;
+        if(freq[i] > 0 && num > 0){
+            result += num;
+        }
+    }
+    //if we haven't found any positive numbers, 
+    //then we return the largest one from the array.
+    if(result != max_num && result == 0){
+        return max_num;
+    }
+    return result;
+}
