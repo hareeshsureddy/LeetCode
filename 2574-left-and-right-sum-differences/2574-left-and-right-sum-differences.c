600f26e1c7254fecd0ef23da6e8fa5605d5c42da
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-07-30 07:36:45 +0530

    Easy 0 ms (100%) 13.7 MB (40.98%) 07-30-2025

--- 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.c
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.c
@@ -4,6 +4,8 @@
 int* leftRightDifference(int* nums, int numsSize, int* returnSize) {
     int *res=(int*)malloc(sizeof(int)*numsSize);
     *returnSize=numsSize;
+    //metode-1
+/*
     for(int i=0;i<numsSize;i++){
         int leftSum=0,rightSum=0,j=0;
         while(j<i){
@@ -14,6 +16,16 @@ int* leftRightDifference(int* nums, int numsSize, int* returnSize) {
             rightSum+=nums[j++];
         }
         res[i]=abs(rightSum-leftSum);
+    }
+*/
+    for(int i=1;i<numsSize;i++){
+        nums[i]+=nums[i-1];
+    }
+    res[0]=nums[numsSize-1]-nums[0];
+    if(numsSize>1)
+    res[numsSize-1]=nums[numsSize-2];
+    for(int i=1;i<numsSize-1;i++){
+        res[i]=abs(nums[numsSize-1]-nums[i-1]-nums[i]);
     }
     return res;
 }
