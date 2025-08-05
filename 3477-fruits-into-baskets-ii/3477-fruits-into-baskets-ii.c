f5ed37722d33607972d79cd2b99af6c97d2b5e57
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-08-05 23:09:45 +0530

    Easy 0 ms (100%) 9.8 MB (97.37%) 08-05-2025

--- 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.c
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.c
@@ -1,14 +1,14 @@
 int numOfUnplacedFruits(int* fruits, int fruitsSize, int* baskets, int basketsSize) {
     int numOfUnplacedFruits=0;
     for(int i=0;i<fruitsSize;i++){
+        int f=fruits[i];
         for(int j=0;j<fruitsSize;j++){
-            if(baskets[j] && fruits[i]<=baskets[j]){
+            if(f<=baskets[j]){
                 baskets[j]=0;
-                fruits[i]=0;
+                numOfUnplacedFruits--;
                 break;
             }
         }
-        if(fruits[i]) numOfUnplacedFruits++;
     }
-    return numOfUnplacedFruits;
+    return fruitsSize+numOfUnplacedFruits;
 }
