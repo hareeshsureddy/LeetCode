8165bc89234b1bdfefbb43c8d9cd4f0a7d3f31b1
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-08-08 00:51:03 +0530

    Medium 14 ms (38.4%) 68.8 MB (69.6%) 08-08-2025

--- 0739-daily-temperatures/0739-daily-temperatures.c
+++ 0739-daily-temperatures/0739-daily-temperatures.c
@@ -1,6 +1,8 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+    //Brute force
+    /*
 int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize) {
     for (int i=0;i<temperaturesSize;i++){
         int days=0;
@@ -14,4 +16,21 @@ int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize)
     }
     *returnSize=temperaturesSize;
     return temperatures;
+} */
+
+int* dailyTemperatures(int* temperatures, int temperaturesSize, int* returnSize) {
+    int stack[100];
+    int index=0;
+    int *res=(int*)malloc(sizeof(int)*temperaturesSize);
+    res[temperaturesSize-1]=0;
+    stack[index++]=temperaturesSize-1;
+    for(int i=temperaturesSize-2;i>=0;i--){
+        while(index>0 && temperatures[i]>=temperatures[stack[index-1]]){
+            index--;
+        }
+        res[i]=(index>0)?stack[index-1]-i:0;
+        stack[index++]=i;
+    }
+    *returnSize=temperaturesSize;
+    return res;
 }
