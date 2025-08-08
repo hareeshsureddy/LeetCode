857a46cd317cd6264a53ced96c3409bb69ab0011
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-08-08 23:12:50 +0530

    Medium 7 ms (41.2%) 98.4 MB (86.52%) 08-08-2025

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.c
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.c
@@ -1,6 +1,6 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
- */
+ */ /*
 int* pivotArray(int* nums, int numsSize, int pivot, int* returnSize) {
     int *res=(int*)malloc(sizeof(int)*numsSize);
     int index=0,equPivot=0;
@@ -14,6 +14,29 @@ int* pivotArray(int* nums, int numsSize, int pivot, int* returnSize) {
         for(int i=0;i<numsSize;i++){
         if(nums[i]>pivot)
         res[index++]=nums[i];
+    }
+    *returnSize=numsSize;
+    return res;
+} */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* pivotArray(int* nums, int numsSize, int pivot, int* returnSize) {
+    int *res=(int*)malloc(sizeof(int)*numsSize);
+    int index=0,equPivot=0,lessPivot=0;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]<pivot)
+        lessPivot++;
+        else if(nums[i]==pivot) equPivot++;
+    }
+    equPivot+=lessPivot;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]<pivot)
+        res[index++]=nums[i];
+        else if(nums[i]>pivot)
+        res[equPivot++]=nums[i];
+        else
+        res[lessPivot++]=pivot;
     }
     *returnSize=numsSize;
     return res;
