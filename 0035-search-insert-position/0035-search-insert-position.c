f17835cb7afd9372b5078e269f4445053822de44
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-08-11 23:50:44 +0530

    Easy 0 ms (100%) 8.4 MB (10.45%) 08-11-2025

---
+++ 0035-search-insert-position/0035-search-insert-position.c
@@ -0,0 +1,16 @@
+int searchInsert(int* nums, int numsSize, int target) {
+    int l=0,r=numsSize-1,mid;
+    while(l<=r){
+        mid=(l+r)/2;
+        if(nums[mid]==target) return mid;
+        if(nums[mid]<target)
+        l=mid+1;
+        else
+        r=mid-1;
+    }
+    printf("%d",mid);
+    if(nums[mid]<target)
+        return mid+1;
+        else
+        return mid;
+}
