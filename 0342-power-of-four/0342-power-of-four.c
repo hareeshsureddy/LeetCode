ea2838da9c98e24b215ec365036c0449854a25ac
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-08-15 13:04:47 +0530

    Easy 0 ms (100%) 7.7 MB (91.5%) 08-15-2025

--- 0342-power-of-four/0342-power-of-four.c
+++ 0342-power-of-four/0342-power-of-four.c
@@ -1,9 +1,9 @@
 bool isPowerOfFour(int n) {
     if(n<1) return false;
     return ((!(n&(n-1))) && (n&0x55555555));
-    if((n&(n-1))) return false;
+  /*  if((n&(n-1))) return false;
     else
     if(n&0x55555555) return true;
     else
-    return false;
+    return false;*/
 }
