c08c70dde245172b116bac9828f376a36df8e4da
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-08-16 16:14:12 +0530

    Easy 0 ms (100%) 7.9 MB (68.52%) 08-16-2025

--- 1323-maximum-69-number/1323-maximum-69-number.c
+++ 1323-maximum-69-number/1323-maximum-69-number.c
@@ -1,6 +1,31 @@
 int maximum69Number (int num) {
-    char *res=(char*)malloc(sizeof(char)*6);
-    sprintf(res,"%d",num);
+    int temp=num,cnt=0;
+    while(temp){
+            cnt++;
+            temp/=10;
+    }
+    uint8_t *res=(uint8_t*)malloc(sizeof(uint8_t)*(cnt));
+    int i=cnt-1;
+   for(;i>=0;i--){
+        res[i]=num%10;
+        num/=10;
+   }
+   i++;
+   while(i<cnt){
+    if(res[i]==6){
+        res[i]=9;
+        break;
+    }
+    i++;
+   }
+   num=0,i=0;
+   for(;i<cnt;i++){
+    num*=10;
+    num+=res[i];
+   }
+   free(res);
+   return num;
+   /* sprintf(res,"%d",num);
     int i=0;
     while(res[i]){
         if(res[i]=='6'){
@@ -10,4 +35,5 @@ int maximum69Number (int num) {
         i++;
     }
     return atoi(res);
+    */
 }
