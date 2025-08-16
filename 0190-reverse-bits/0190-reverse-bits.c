7d5ec6b04c05d30ddccde01967c2c30840105a8f
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-08-16 15:50:38 +0530

    Easy 2 ms (48.09%) 8.2 MB (14.31%) 08-16-2025

--- 0190-reverse-bits/0190-reverse-bits.c
+++ 0190-reverse-bits/0190-reverse-bits.c
@@ -1,9 +1,15 @@
 uint32_t reverseBits(uint32_t n) {
-    uint32_t result=0;
+   /* uint32_t result=0;
     for (int i=0;i<32;i++){
         result<<=1;
         result|=(n&1);
         n>>=1;
     }
-    return result;
+    return result;*/
+    n=((n>>1)&0x55555555)|((n&0x55555555)<<1);
+    n=((n>>2)&0x33333333)|((n&0x33333333)<<2);
+    n=((n>>4)&0x0F0F0F0F)|((n&0x0F0F0F0F)<<4);
+    n=((n>>8)&0x00FF00FF)|((n&0x00FF00FF)<<8);
+    n=(n>>16)|(n<<16);
+    return n;
 }
