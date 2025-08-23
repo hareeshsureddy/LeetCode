3e7f53337d9a86194012390fabb637a1a9786681
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-08-24 00:53:29 +0530

    Medium 0 ms (100%) 8.4 MB (85.54%) 08-24-2025

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.c
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.c
@@ -1,7 +1,45 @@
-/* char* reverseWords(char* s) {
+void myreverseWords(char* s, int i, int j) {
+    while(i<j){
+        char temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        i++;
+        j--;
+    }
+}
 
-} */
-
+char* reverseWords(char* s) {
+    int len=strlen(s);
+    int i=0,j=len-1;
+    while(i<j){
+        char temp=s[i];
+        s[i]=s[j];
+        s[j]=temp;
+        i++;
+        j--;
+    }
+    i=0,j=0;
+    while(j<len){
+        while(j<len && s[j]==' ') j++;
+        while(j<len && s[j]!=' '){
+            s[i++]=s[j++];
+        }
+        s[i++]=' ';
+    }
+    while(s[i-1]==' ') i--;
+    s[i]='\0';
+    len=i;
+    i=0,j=0;
+    while(j<len){
+        while(j<len && s[j]!=' ') j++;
+        myreverseWords(s,i,j-1);
+        j++;
+        i=j;
+    }
+    myreverseWords(s,i,j-1);
+    return s;
+}
+/*
 char* reverseWords(char* s) {
     int len=strlen(s);
     char *result=(char*)malloc((len+1)*sizeof(char));
@@ -18,4 +56,4 @@ char* reverseWords(char* s) {
     }
     result[idx-1]='\0';
     return result;
-} 
+} */
