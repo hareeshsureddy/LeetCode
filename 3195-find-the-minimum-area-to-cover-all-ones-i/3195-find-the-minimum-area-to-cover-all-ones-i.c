5b1c0fc5e96b4f1130908d62bcee9a9c15d8e40d
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-08-24 01:41:42 +0530

    Medium 458 ms (5.72%) 55.6 MB (74.93%) 08-24-2025

---
+++ 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.c
@@ -0,0 +1,37 @@
+int minimumArea(int** grid, int gridSize, int* gridColSize) {
+        int maxRow=0,minRow=INT_MAX,maxCol=0,minCol=INT_MAX;
+        for (int i=0;i<gridSize;i++){
+            for(int j=0;j<*gridColSize;j++){
+                if(grid[i][j]){
+                    maxRow=fmax(maxRow,i);
+                    maxCol=fmax(maxCol,j);
+                    minRow=fmin(minRow,i);
+                    minCol=fmin(minCol,j);
+                }
+            }
+        }
+        return (maxRow-minRow+1)*(maxCol-minCol+1);
+}
+
+//271 / 712 testcases
+/*
+int minimumArea(int** grid, int gridSize, int* gridColSize) {
+    int R=0,C=0;
+    for(int i=0;i<gridSize;i++){
+        for(int j=0;j<*gridColSize;j++){
+            if(grid[i][j]){
+                R++;
+                break;
+            }
+        }
+    }
+    for(int i=0;i<*gridColSize;i++){
+        for(int j=0;j<gridSize;j++){
+            if(grid[j][i]){
+                 C++;
+                break;
+            }
+        }
+    }
+    return R*C;
+}*/
