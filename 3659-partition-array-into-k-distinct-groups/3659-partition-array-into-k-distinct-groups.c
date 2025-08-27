a2a6763a9e7517c58064a9fbbc80df583dc7fc28
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-08-27 10:52:56 +0530

    Medium 69 ms (6.3%) 28.9 MB (27.95%) 08-27-2025

---
+++ 3659-partition-array-into-k-distinct-groups/3659-partition-array-into-k-distinct-groups.c
@@ -0,0 +1,17 @@
+int compare(const void *a, const void *b){
+    return *(int*)a-*(int*)b;
+}
+
+bool partitionArray(int* nums, int numsSize, int k) {
+    if(numsSize%k!=0) return false;
+    qsort(nums,numsSize,sizeof(int),compare);
+    int i=0,j=0;
+    int freq=numsSize/k;
+    while(j<numsSize){
+        while(j<numsSize && nums[i]==nums[j]) j++;
+        if(j-i>freq) return false;
+        i=j;
+    }
+   // if(j-i>k) return false;
+    return true;
+}
