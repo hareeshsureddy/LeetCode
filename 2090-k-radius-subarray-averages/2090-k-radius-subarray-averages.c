405a7aecf65879b35763b861fa143524503b5c7a
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-08-27 17:50:13 +0530

    Medium 4 ms (88.89%) 102.1 MB (100%) 08-27-2025

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.c
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.c
@@ -1,6 +1,33 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+
+ int* getAverages(int* nums, int numsSize, int k, int* returnSize) {
+    *returnSize=numsSize;
+    if(!k) return nums;
+    int *result=(int*)malloc(numsSize*sizeof(int));
+    memset(result,-1,sizeof(int)*numsSize);
+    if(numsSize<(2*k+1)) return result;
+    long long PrefixSum=0;
+    const int divisor=(k*2+1);
+    int right=(k*2);
+    int left=0;
+    int i=k;
+    for(int j=left;j<numsSize && j<=right;j++){
+    PrefixSum+=nums[j];
+    }
+    result[i++]=PrefixSum/divisor; 
+    right++; 
+    while(right<numsSize){ 
+        PrefixSum=PrefixSum-nums[left]+nums[right];
+        result[i++]=PrefixSum/divisor;
+        left++;
+        right++;
+    }
+    return result;
+}
+
+ /*
 int* getAverages(int* nums, int numsSize, int k, int* returnSize) {
     int *result=(int*)calloc(numsSize,sizeof(int));
     *returnSize=numsSize;
@@ -23,7 +50,7 @@ int* getAverages(int* nums, int numsSize, int k, int* returnSize) {
         result[i]=-1;
     }
     return result;
-}
+} */
  /*
 int* getAverages(int* nums, int numsSize, int k, int* returnSize) {
     long long *res=(long long*)calloc(numsSize,sizeof(long long));
