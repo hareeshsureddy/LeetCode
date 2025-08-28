67db453254df7b0dbc907f3483aa975ea70a864d
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-08-28 22:41:01 +0530

    Medium 0 ms (100%) 13.2 MB (87.5%) 08-28-2025

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.c
@@ -0,0 +1,38 @@
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+  int compareLH(const void *a, const void *b){
+    return *(int*)a-*(int*)b;
+ }
+int** diagonalSort(int** grid, int gridSize, int* gridColSize, int* returnSize, int** returnColumnSizes) {
+    int *temp=(int*)malloc(sizeof(int)*gridSize);
+    for(int i=0;i<gridSize;i++){
+        int j=0;
+        int k=i;
+        int index=0;
+        while(k<gridSize && j<*gridColSize){
+            temp[index++]=grid[k++][j++];
+        }
+        qsort(temp,index,sizeof(int),compareLH);
+        while(index>0){
+            grid[--k][--j]=temp[--index];
+        }
+    }
+    for(int i=1;i<*gridColSize;i++){
+        int j=i;
+        int k=0;
+        int index=0;
+        while(k<gridSize && j<*gridColSize){
+            temp[index++]=grid[k++][j++];
+        }
+        qsort(temp,index,sizeof(int),compareLH);
+        while(index>0){
+            grid[--k][--j]=temp[--index];
+        }
+    }
+    *returnSize=gridSize;
+    *returnColumnSizes=gridColSize;
+    return grid;
+}
