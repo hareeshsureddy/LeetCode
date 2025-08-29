61dbc6397c85dadd74b21724d99c56663bfac6bf
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-08-30 02:10:56 +0530

    Easy 4 ms (44%) 10.3 MB (72%) 08-30-2025

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.c
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.c
@@ -2,8 +2,8 @@ int minDeletionSize(char** strs, int strsSize) {
     int cnt=0;
     int len=strlen(strs[0]);
     for(int i=0;i<len;i++){
-        for(int j=0;j<strsSize-1;j++){
-            if(strs[j][i]>strs[j+1][i]){
+        for(int j=1;j<strsSize;j++){
+            if(strs[j][i]<strs[j-1][i]){
                 cnt++;
                 break;
             }
