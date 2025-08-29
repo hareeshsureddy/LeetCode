1341c92df54def947bc7f480dd7a22d459ed3960
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-08-30 01:30:41 +0530

    Medium 0 ms (100%) 8.1 MB (55.56%) 08-30-2025

--- 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.c
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.c
@@ -1,11 +1,11 @@
 long long flowerGame(int n, int m) {
-           long long res=0;
-        /*   for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if((i+j)%2!=0) res++;
-             }
-           }
-        */
-           return (1LL)*(n/2)*((m+1)/2)+(1LL)*(m/2)*((n+1)/2);
-           //return res;
+    long long res = 0;
+    /*   for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            if((i+j)%2!=0) res++;
+         }
+       }
+       return res;
+    */
+    return (long long) (n / 2) * ((m + 1) / 2) + (long long) (m / 2) * ((n + 1) / 2);
 }
