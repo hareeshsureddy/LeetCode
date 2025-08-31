bfc171f36d8e257b5ebd3755c99c66a9b19f2c59
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-08-31 23:28:26 +0530

    Medium 0 ms (100%) 9.4 MB (45.69%) 08-31-2025

--- 0036-valid-sudoku/0036-valid-sudoku.c
+++ 0036-valid-sudoku/0036-valid-sudoku.c
@@ -1,3 +1,31 @@
+bool isValid(char **board,int row, int coloum, char c){
+        for(int i=0;i<9;i++){
+            if(board[row][i]==c) return false;
+            if(board[i][coloum]==c) return false;
+        }
+        int start_i=(row/3)*3;
+        int start_j=(coloum/3)*3;
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                if(board[start_i+i][start_j+j]==c) return false;
+            }
+        }
+        return true;
+}
+bool isValidSudoku(char** board, int boardSize, int* boardColSize) {
+    for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+            if(board[i][j]!='.'){
+               char c=board[i][j];
+               board[i][j]='.';
+               if(!isValid(board,i,j,c)) return false;
+               board[i][j]=c;
+            }
+        }
+    }
+    return true;
+}
+/*
 bool isValidSudoku(char** board, int boardSize, int* boardColSize) {
     uint8_t map[9]={0};
     for(int i=0;i<9;i++){
@@ -29,4 +57,4 @@ bool isValidSudoku(char** board, int boardSize, int* boardColSize) {
         }
     } 
     return true;
-}
+} */
