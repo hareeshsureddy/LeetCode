66c33fe717ddae8ac162732b7cca7c51bbe6658b
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-09-01 19:28:14 +0530

    Medium 10 ms (60%) 18.9 MB (100%) 09-01-2025

--- 3006-find-beautiful-indices-in-the-given-array-i/3006-find-beautiful-indices-in-the-given-array-i.c
+++ 3006-find-beautiful-indices-in-the-given-array-i/3006-find-beautiful-indices-in-the-given-array-i.c
@@ -1,6 +1,7 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+ /*
 static uint32_t i_index[100001];
 static uint32_t j_index[100001];
 // Binary search lower_bound in C
@@ -62,6 +63,7 @@ int* beautifulIndices(char* s, char* a, char* b, int k, int* returnSize) {
             }
         }
     } */
+    /*
     int right=j_index[index_j-1];
     int left=j_index[0];
         for(int i=0;i<index_i;i++){
@@ -75,4 +77,123 @@ int* beautifulIndices(char* s, char* a, char* b, int k, int* returnSize) {
     *returnSize=index;
     return res;
 
+} */
+
+static void buildNexts(
+	const char * const needle,
+	const int needleLen,
+	int * const nexts
+){
+	assert(needleLen >= 1);
+
+	nexts[0] = 0;
+	for (int i = 1; i < needleLen; i += 1){
+		int last = nexts[i - 1];
+		while (1){
+			if (needle[last] == needle[i]){
+				nexts[i] = last + 1;
+				break;
+			}
+
+			if (0 == last){
+				nexts[i] = 0;
+				break;
+			}
+
+			last = nexts[last - 1];
+		}
+	}
+}
+
+int * strStrAll(
+	const char * const hayStack,
+	const char * const needle,
+
+	int * const pRetsLen
+){
+	const int needleLen = strlen(needle);
+	assert(needleLen >= 1);
+	int nexts[needleLen];
+	buildNexts(needle, needleLen, nexts);
+
+	const int hayStackLen = strlen(hayStack);
+
+	int * const rets = (int *)malloc(sizeof (int) * hayStackLen);
+	*pRetsLen = 0;
+
+	for (int i = 0, j = 0; i < hayStackLen; ){
+		if (hayStack[i] == needle[j]){
+			i += 1;
+			j += 1;
+
+			if (needleLen == j){
+				rets[*pRetsLen] = i - j;
+				*pRetsLen += 1;
+
+				j = nexts[j - 1];
+			}
+		}else {
+			if (0 == j){
+				i += 1;
+			}else {
+				j = nexts[j - 1];
+			}
+		}
+	}
+
+	return rets;
+}
+
+//-------- --------
+
+int * beautifulIndices(
+	const char * const hayStack,
+	const char * const needle1,
+	const char * const needle2,
+	const int maxDiff,
+
+	int * const pRetsLen //out
+){
+	int needle2IdxsLen;
+	int * needle2Idxs = strStrAll(hayStack, needle2, &needle2IdxsLen);
+
+	int needle1IdxsLen;
+	int * needle1Idxs = strStrAll(hayStack, needle1, &needle1IdxsLen);
+
+	int * const rets = (int *)malloc(sizeof (int) * needle1IdxsLen);
+	*pRetsLen = 0;
+
+	int first = 0, nextOfLast = 0;
+	for (int i = 0; i < needle1IdxsLen; i += 1){
+		const int cur = needle1Idxs[i];
+
+		const int min = cur - maxDiff;
+		while (
+			first < needle2IdxsLen &&
+			needle2Idxs[first] < min
+		){
+			first += 1;
+		}
+
+		const int max = cur + maxDiff;
+		while (
+			nextOfLast < needle2IdxsLen &&
+			needle2Idxs[nextOfLast] <= max
+		){
+			nextOfLast += 1;
+		}
+
+		if (nextOfLast - first >= 1){
+			rets[*pRetsLen] = cur;
+			*pRetsLen += 1;
+		}
+	}
+
+	free(needle2Idxs);
+	needle2Idxs = NULL;
+
+	free(needle1Idxs);
+	needle1Idxs = NULL;
+
+	return rets;
 }
