f00e5743853336765a5edf9dc2c4232d72930ffc
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-09-01 15:49:50 +0530

    Medium 9 ms (29.41%) 10.8 MB (80.88%) 09-01-2025

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.c
@@ -0,0 +1,13 @@
+int minSteps(char* s, char* t) {
+    int map1[26];
+    int map2[26];
+    int i=0,cnt=0;
+    while(s[i]){
+        map1[s[i]-'a']++;
+        map2[t[i++]-'a']++;
+    }
+    for(int i=0;i<26;i++){
+        cnt+=abs(map1[i]-map2[i]);
+    }
+    return cnt/2;
+}
