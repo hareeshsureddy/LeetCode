09b528b2434bfaaa77e3d0cd3bc93332d56688d5
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-09-02 01:45:35 +0530

    Medium 43 ms (100%) 69.1 MB (75%) 09-02-2025

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.c
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.c
@@ -13,13 +13,14 @@ int* sortJumbled(int* mapping, int mappingSize, int* nums, int numsSize, int* re
     *returnSize=numsSize;
     jumbleData *jData=(jumbleData*)calloc(sizeof(jumbleData),numsSize);
     for(int i=0;i<numsSize;i++){
+        /*
         char num[11];
         sprintf(num,"%d",nums[i]);
         for(int i=0;num[i];i++){
             num[i]=mapping[num[i]-'0']+'0';
         }
         jData[i].data=atoi(num);
-        /*
+        */
         int num=nums[i];
         if(!num){
             jData[i].data=(mapping[0]);
@@ -31,7 +32,6 @@ int* sortJumbled(int* mapping, int mappingSize, int* nums, int numsSize, int* re
             tens*=10;
         }
         }
-        */
         jData[i].index=i;
     }
     qsort(jData,numsSize,sizeof(jumbleData),compare);
