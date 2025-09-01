3f13982884f39b2419e2e1a8df11477fe434ca1d
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-09-02 01:35:04 +0530

    Medium 59 ms (75%) 69 MB (75%) 09-02-2025

---
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.c
@@ -0,0 +1,35 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+ typedef struct{
+    int data;
+    int index;
+ }jumbleData;
+
+ int compare(const void *a, const void *b){
+     return ((jumbleData*)a)->data-((jumbleData*)b)->data;
+ }
+int* sortJumbled(int* mapping, int mappingSize, int* nums, int numsSize, int* returnSize) {
+    *returnSize=numsSize;
+    jumbleData *jData=(jumbleData*)calloc(sizeof(jumbleData),numsSize);
+    for(int i=0;i<numsSize;i++){
+        int num=nums[i];
+        if(!num){
+            jData[i].data=(mapping[0]);
+        }else{
+        int tens=1;
+        while(num){
+            jData[i].data+=(mapping[num%10]*tens);
+            num/=10;
+            tens*=10;
+        }
+        }
+        jData[i].index=i;
+    }
+    qsort(jData,numsSize,sizeof(jumbleData),compare);
+    int *res=(int*)malloc(sizeof(int)*numsSize);
+    for(int i=0;i<numsSize;i++){
+        res[i]=nums[jData[i].index];
+    }
+    return res;
+}
