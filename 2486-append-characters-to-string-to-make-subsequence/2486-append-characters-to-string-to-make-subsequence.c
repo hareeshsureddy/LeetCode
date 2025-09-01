100fd1d691ab51c3c873a1b96012398f12db59a9
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-09-02 00:43:34 +0530

    Medium 3 ms (16.67%) 9.4 MB (33.33%) 09-02-2025

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.c
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.c
@@ -2,8 +2,21 @@ int appendCharacters(char* s, char* t) {
     if (strstr(s,t)) return 0;
     int len=strlen(s);
     int len1=strlen(t);
-    int i,j,max=0;
+    int i=0,j=0;
     int cnt=0;
+    while(i<len1){
+        while(j<len){
+            if(s[j]==t[i]){
+                cnt++;
+                j++;
+                break;
+            }
+            j++;
+        }
+        if(j==len) break;
+        i++;
+    }
+    /*
     for( i=0;i<len;i++){
         for( j=0+cnt;j<len1;j++){
             if(j<len1 && i+j<len && s[i+j]==t[j])
@@ -11,7 +24,7 @@ int appendCharacters(char* s, char* t) {
             else
             break;
         }
-        max=(max<cnt)?cnt:max;
-    }
-    return len1-max;
+    } 
+    */
+    return len1-cnt;
 }
