916140d84d16e1c364bd46b49a59e4bdd6cdaf13
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-09-20 00:14:15 +0530

    Easy 0 ms (100%) 19.6 MB (73.58%) 09-20-2025

--- 0997-find-the-town-judge/0997-find-the-town-judge.c
+++ 0997-find-the-town-judge/0997-find-the-town-judge.c
@@ -8,16 +8,20 @@ int findJudge(int n, int** trust, int trustSize, int* trustColSize) {
     }
     if(cnt<trustSize-1) return -1;
     return trust[0][1]; */
-    int *map=(int*)calloc(sizeof(int),n);
-    int *map1=(int*)calloc(sizeof(int),n);
+    int *map=(int*)calloc(sizeof(int),n+1);
+    int *map1=(int*)calloc(sizeof(int),n+1);
     for(int i=0;i<trustSize;i++){
-        map[trust[i][0]-1]++; // Person cnt for judge = 0
-        map1[trust[i][1]-1]++; // Judge cnt should equal to n-1;
+        map[trust[i][0]]++; // Person cnt for judge = 0
+        map1[trust[i][1]]++; // Judge cnt should equal to n-1;
     }
-    for(int i=0;i<n;i++){
-       if(map[i]) continue;
-       if(map1[i]==n-1) return i+1;
-       return -1;
+    for(int i=1;i<=n;i++){
+       if(!map[i] && map1[i]==n-1) {
+            free(map);
+            free(map1);
+        return i;
+       }
     }
+    free(map);
+    free(map1);
     return -1;
 }
