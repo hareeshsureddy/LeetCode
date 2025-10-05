e429759a8e713ae0421656427779ee545fc37463
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-10-05 21:57:45 +0530

    Easy 1 ms (19.4%) 9.2 MB (90.48%) 10-05-2025

---
+++ 0605-can-place-flowers/0605-can-place-flowers.c
@@ -0,0 +1,26 @@
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
+    if(flowerbedSize==1){
+        if(flowerbed[0]){
+            if(n==0) return true;
+            return false;
+        }else{
+            if(n<2) return true;
+            return false;
+        }
+    }
+    int cnt=0;
+    if(!flowerbed[0] && !flowerbed[1]){
+        flowerbed[0]=1;
+        cnt++;
+    }
+    for(int i=1;i<flowerbedSize-1;i++){
+        if(!flowerbed[i-1] && !flowerbed[i] && !flowerbed[i+1]){
+                flowerbed[i]=1;
+                cnt++;
+        }
+    }
+    if(!flowerbed[flowerbedSize-1] && !flowerbed[flowerbedSize-2])
+        cnt++;
+    if(cnt>=n) return true;
+    return false;
+}
