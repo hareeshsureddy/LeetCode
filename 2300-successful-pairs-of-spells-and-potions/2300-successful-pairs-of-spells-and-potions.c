2bfb23a6b7b1dddc7626c0200006f3a5b3d3cdaa
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-10-09 00:26:00 +0530

    Medium 56 ms (50%) 70.1 MB (13.04%) 10-09-2025

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.c
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.c
@@ -24,28 +24,28 @@ int binarySearch(int* arr, int start, int end, int target) {
             idx=mid;
         }else{
             l=mid+1;
-        }*/
+        } */
     }
     return idx;
 }
 int* successfulPairs(int* spells, int spellsSize, int* potions, int potionsSize,
                      long long success, int* returnSize) {
-    int* res = (int*)malloc( spellsSize*sizeof(int));
+    int* res = (int*)malloc(spellsSize * sizeof(int));
     qsort(potions, potionsSize, sizeof(int), compare);
     int maxtarget = potions[potionsSize - 1];
     for (int i = 0; i < spellsSize; i++) {
-    /*    double num = success / (double)spells[i];
-        int target = (int)num;
-        target = (num > target) ? target + 1 : target;*/
-         long long target = (success + spells[i] - 1) / spells[i]; // ceil integer
-        if (target > maxtarget){
-            res[i] =0;
+        /*    double num = success / (double)spells[i];
+            int target = (int)num;
+            target = (num > target) ? target + 1 : target;*/
+        long long target =
+            (success + spells[i] - 1) / spells[i]; // ceil integer
+        if (target > maxtarget) {
+            res[i] = 0;
             continue;
         }
         int idx = binarySearch(potions, 0, potionsSize - 1, target);
-        printf("%d ",idx);
-        if(idx>=0)
-        res[i] = potionsSize - idx;
+        if (idx >= 0)
+            res[i] = potionsSize - idx;
     }
     *returnSize = spellsSize;
     return res;
