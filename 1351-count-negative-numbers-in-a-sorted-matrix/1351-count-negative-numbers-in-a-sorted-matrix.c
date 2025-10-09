bdc23ffe210a6aab75baa3fe1fbb6dac2abe87f3
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-10-09 23:37:32 +0530

    Easy 0 ms (100%) 10 MB (13.01%) 10-09-2025

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.c
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.c
@@ -1,11 +1,24 @@
 int countNegatives(int** grid, int gridSize, int* gridColSize) {
-    int cnt=0;
+    int cnt = 0;
+    //O(m+n)
+    int r=gridSize-1;
+    int c=0;
+    while(r>=0 && c<*gridColSize){
+        if(grid[r][c]<0){
+            cnt+=(*gridColSize-c);
+            r--;
+        }else{
+            c++;
+        }
+    }
+    // O(m*n)
+    /*
     for(int j=0;j<gridSize;j++){
     for(int i=*gridColSize-1;i>=0;i--){
         if(grid[j][i]<0) cnt++;
         else
         break;
     }
-    }
+    }*/
     return cnt;
 }
