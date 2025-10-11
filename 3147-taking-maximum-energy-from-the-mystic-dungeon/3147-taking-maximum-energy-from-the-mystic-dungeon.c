9be7a522b4a64fa4ad1b6a9ff2867c8c8fe3fe9a
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-10-11 19:47:38 +0530

    Medium 153 ms (80.95%) 18.2 MB (14.29%) 10-11-2025

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.c
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.c
@@ -1,4 +1,16 @@
 int maximumEnergy(int* energy, int energySize, int k) {
+    int dp[energySize];
+    int result = INT_MIN;
+    for (int i = energySize - 1; i >= 0; --i) {
+        int next = (i + k < energySize) ? dp[i + k] : 0;
+        dp[i] = energy[i] + next;
+        if (dp[i] > result)
+            result = dp[i];
+    }
+    return result;
+}
+/*
+int maximumEnergy(int* energy, int energySize, int k) {
     int maxEnergy=INT_MIN;
     for(int i=0;i<energySize-k;i++){
         int idx=i;
@@ -12,4 +24,4 @@ int maximumEnergy(int* energy, int energySize, int k) {
     for(int i=energySize-k;i<energySize;i++)
     maxEnergy=(maxEnergy<energy[i])?energy[i]:maxEnergy;
     return maxEnergy;
-}
+}*/
