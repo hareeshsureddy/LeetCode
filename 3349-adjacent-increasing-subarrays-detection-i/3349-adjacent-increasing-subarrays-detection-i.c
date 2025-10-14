0fa1eb3356f52c6927556cebd10d26d4c39ad67d
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-10-14 23:52:32 +0530

    Easy 3 ms (100%) 9.2 MB (50%) 10-14-2025

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.c
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.c
@@ -1,3 +1,4 @@
+/*
 bool hasIncreasingSubarrays(int* nums, int numsSize, int k) {
     for (int i = 0; i <= numsSize - (2 * k); i++) {
         int j = i;
@@ -18,5 +19,22 @@ bool hasIncreasingSubarrays(int* nums, int numsSize, int k) {
             if (j == upperBound)  return true;
         }
     }
+    return false;
+}
+*/
+bool hasIncreasingSubarrays(int* nums, int numsSize, int k) {
+    int prevLen=1,currentLen=1;
+    for(int i=1;i<numsSize;i++){
+        if(nums[i]>nums[i-1]){
+            currentLen++;
+            continue;
+        }
+        if(currentLen/2>=k) return true;
+        if(fmin(currentLen,prevLen)>=k) return true;
+        prevLen=currentLen;
+        currentLen=1;
+    }
+    if(currentLen/2>=k) return true;
+    if(fmin(currentLen,prevLen)>=k) return true;
     return false;
 }
