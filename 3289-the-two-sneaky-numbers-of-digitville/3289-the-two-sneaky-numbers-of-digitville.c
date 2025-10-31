297aa85c8aab16efdb5063c29fcdc719fcfea859
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-11-01 01:24:01 +0530

    Easy 2 ms (13.82%) 10 MB (95.12%) 11-01-2025

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.c
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.c
@@ -1,15 +1,20 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+int compare(const void *a, const void *b){
+    return *(int*)a-*(int*)b;
+}
 int* getSneakyNumbers(int* nums, int numsSize, int* returnSize) {
-    int map[101]={0};
+    qsort(nums,numsSize,sizeof(int),compare);
+    int *res=(int*)malloc(sizeof(int)*2);
     *returnSize=0;
-    for(int i=0;i<numsSize;i++){
-        map[nums[i]]++;
-        if(map[nums[i]]>1){
-            nums[(*returnSize)++]=nums[i];
-            if(*returnSize==2) return nums;
-        } 
+    for(int i=0;i<numsSize-1;i++){
+        if(nums[i]==nums[i+1]){
+            res[*returnSize]=nums[i];
+            (*returnSize)++;
+            if(*returnSize==2)
+            return res;
+        }
     }
-    return nums;
+    return 0;
 }
