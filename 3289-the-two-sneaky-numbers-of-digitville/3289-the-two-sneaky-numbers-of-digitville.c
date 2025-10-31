1b3cd96c3bb08731bc77075e39a504b9a44e8222
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-11-01 01:32:57 +0530

    Easy 0 ms (100%) 10 MB (95.12%) 11-01-2025

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.c
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.c
@@ -18,3 +18,18 @@ int* getSneakyNumbers(int* nums, int numsSize, int* returnSize) {
     }
     return 0;
 }
+
+/*
+int* getSneakyNumbers(int* nums, int numsSize, int* returnSize) {
+    int map[101]={0};
+    *returnSize=0;
+    for(int i=0;i<numsSize;i++){
+        map[nums[i]]++;
+        if(map[nums[i]]>1){
+            nums[(*returnSize)++]=nums[i];
+            if(*returnSize==2) return nums;
+        } 
+    }
+    return nums;
+}
+*/
