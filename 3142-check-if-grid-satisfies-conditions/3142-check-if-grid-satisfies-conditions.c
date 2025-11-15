1e1a401035f5b87bf41e70c30196cb19ad213b94
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-11-15 23:57:06 +0530

    Easy 0 ms (100%) 9.6 MB (4.17%) 11-15-2025

---
+++ 3142-check-if-grid-satisfies-conditions/3142-check-if-grid-satisfies-conditions.c
@@ -0,0 +1,11 @@
+bool satisfiesConditions(int** grid, int gridSize, int* gridColSize) {
+    for(int i=0;i<gridSize;i++){
+        for(int j=0;j<*gridColSize;j++){
+            if(i<gridSize-1)
+            if(grid[i][j]!=grid[i+1][j])  return false;
+             if(j<*gridColSize-1)
+            if(grid[i][j]==grid[i][j+1])  return false;
+        }
+    }
+    return true;
+}
