57d5b3a3abf4825df96120c951792ef5d115aec8
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-11-22 23:34:38 +0530

    Medium 29 ms (66.67%) 16.4 MB (100%) 11-22-2025

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.c
@@ -0,0 +1,18 @@
+int compare(const void *a, const void *b){
+    return *(int*)a-*(int*)b;
+}
+int maximumBags(int* capacity, int capacitySize, int* rocks, int rocksSize, int additionalRocks) {
+    for(int i=0;i<capacitySize;i++){
+        rocks[i]=capacity[i]-rocks[i];
+    }
+    qsort(rocks,rocksSize,sizeof(int),compare);
+    int maximumBags=0;
+    for(int i=0;i<rocksSize;i++){
+            if(rocks[i]<=additionalRocks)
+            additionalRocks-=rocks[i];
+            else
+            return maximumBags;
+            maximumBags++;
+    }
+    return maximumBags;
+}
