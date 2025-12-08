713d34604eaf6dce7ae4c0dd0b66c9463b82d7bb
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-12-09 00:31:21 +0530

    Easy 342 ms (5%) 7.9 MB (25%) 12-09-2025

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.c
@@ -0,0 +1,11 @@
+int countTriples(int n) {
+    int countTriples=0;
+    for (int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            for(int k=1;k<=n;k++){
+                if(pow(i,2)+pow(j,2)==pow(k,2)) countTriples++;
+            }
+        }
+    }
+    return countTriples;
+}
