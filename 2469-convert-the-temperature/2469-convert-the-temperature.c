04a415b7536e3591ebfd871cf7ed7a23fad35399
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-12-20 01:10:53 +0530

    Easy 0 ms (100%) 7.9 MB (46.37%) 12-20-2025

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.c
@@ -0,0 +1,10 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+double* convertTemperature(double celsius, int* returnSize) {
+    double *res=(double*)malloc(sizeof(double)*2);
+    res[0]=celsius+273.15;
+    res[1]=celsius*1.80+32;
+    *returnSize=2;
+    return res;
+}
