6e9ffdb79ee441cee678b56056df24a4697f93aa
Hareesh suReddy <hareeshsureddy@gmail.com>
2025-12-30 00:45:55 +0530

    Medium 1620 ms (5.03%) 7.7 MB (98.49%) 12-30-2025

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.c
@@ -0,0 +1,18 @@
+int findTheWinner(int n, int k) {
+    int queue[n];
+    for(int i=0;i<n;i++)
+    queue[i]=i+1;
+    int i=0,idx;
+    while(n>1){
+        idx=(i+(k-1))%n;
+        i=idx;
+        while(idx<n-1){
+            queue[idx]=queue[idx+1];
+            printf("%d ",queue[idx]);
+            idx++;
+        }
+        printf("\n");
+        n--;
+    }
+    return queue[0];
+}
