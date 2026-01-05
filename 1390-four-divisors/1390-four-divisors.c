68af095e2fe81bab651bddd850d66194612a1158
Hareesh suReddy <hareeshsureddy@gmail.com>
2026-01-05 22:48:42 +0530

    Medium 7 ms (86.36%) 8.6 MB (40.91%) 01-05-2026

--- 1390-four-divisors/1390-four-divisors.c
+++ 1390-four-divisors/1390-four-divisors.c
@@ -1,6 +1,26 @@
 int sumFourDivisors(int* nums, int numsSize) {
     int sumFourDivisors=0;
     for(int i=0;i<numsSize;i++){
+        int num=0,cnt=0;
+        for(int j=2;j*j<=nums[i];j++){
+            if(nums[i]%j==0){
+                int other=nums[i]/j;
+                if(other==j) cnt++; //To skip the nums[i]
+                num=j;
+                cnt++;
+            }
+            if(cnt>1) break;
+        }
+        if(cnt==1){
+            sumFourDivisors+=num+1+nums[i]+(nums[i]/num);
+        }
+    }
+    return sumFourDivisors;
+}
+
+/* int sumFourDivisors(int* nums, int numsSize) {
+    int sumFourDivisors=0;
+    for(int i=0;i<numsSize;i++){
         int len=nums[i]>>1;
         int sum=0,cnt=0;
         for(int j=2;j<=len;j++){
@@ -15,4 +35,4 @@ int sumFourDivisors(int* nums, int numsSize) {
         }
     }
     return sumFourDivisors;
-}
+} */
