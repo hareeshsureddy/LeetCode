7039d71a363ac9ed71ada9ca353cb37cc924434e
Hareesh suReddy <hareeshsureddy@gmail.com>
2026-01-10 23:51:44 +0530

    Easy 0 ms (100%) 7.7 MB (85%) 01-10-2026

--- 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.c
+++ 3274-check-if-two-chessboard-squares-have-the-same-color/3274-check-if-two-chessboard-squares-have-the-same-color.c
@@ -3,6 +3,8 @@ bool checkTwoChessboards(char* coordinate1, char* coordinate2) {
     int r2=coordinate2[1]-'1';
     int c1=coordinate1[0]-'a';
     int c2=coordinate2[0]-'a';
+    //Methode-1
+    /*
     if(c1&1){
         if(r1&1){
             if(r2&1&c2&1 || (r2%2==0 && c2%2==0)) return true;
@@ -24,4 +26,17 @@ bool checkTwoChessboards(char* coordinate1, char* coordinate2) {
             return false;
         }
     }
+    */
+    bool chessboard[8][8];
+    bool boxStatus=false;
+    for(int i=0;i<8;i++){
+        for(int j=0;j<8;j++){
+            chessboard[i][j]=boxStatus;
+            boxStatus=!boxStatus;
+        }
+        boxStatus=!boxStatus;
+    }
+    if(chessboard[r1][c1]==chessboard[r2][c2]) return true;
+    else
+    return false;
 }
