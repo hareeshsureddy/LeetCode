1b7e30c83da7ec03eab6aabded86685a0becfd26
Hareesh suReddy <hareeshsureddy@gmail.com>
2026-01-11 23:31:10 +0530

    Medium 0 ms (100%) 9 MB (78.74%) 01-11-2026

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.c
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.c
@@ -1,6 +1,40 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+
+ #define ENSURE_CAPACITY()  \
+    do{                     \
+            if(idx>=len)    \
+            {               \
+                len*=2;     \
+                char **temp=realloc(res,sizeof(char*)*len); \
+                if(!temp) return NULL; \
+                res=temp;   \
+            }   \
+    }while(0)
+char** buildArray(int* target, int targetSize, int n, int* returnSize) {
+    int len=10;
+    char **res=(char**)malloc(sizeof(char*)*len);
+    int idx=0;
+    int streamNum=1;
+    for(int i=0;i<targetSize;i++){
+        int num=target[i];
+        while(streamNum<num){
+            ENSURE_CAPACITY();
+            res[idx++]=strdup("Push");
+            ENSURE_CAPACITY();
+            res[idx++]=strdup("Pop");
+            streamNum++;            
+        }
+            ENSURE_CAPACITY();
+            res[idx++]=strdup("Push"); 
+            streamNum++;       
+    }
+    *returnSize=idx;
+    return res;
+}
+
+/*
 char** buildArray(int* target, int targetSize, int n, int* returnSize) {
     int len=10;
     char **res=(char**)malloc(sizeof(char*)*len);
@@ -53,3 +87,4 @@ char** buildArray(int* target, int targetSize, int n, int* returnSize) {
     *returnSize=idx;
     return res;
 }
+*/
