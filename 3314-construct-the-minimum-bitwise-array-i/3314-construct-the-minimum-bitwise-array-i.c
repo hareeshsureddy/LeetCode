7d4cf9ed725c3a01e5fb56a6ae3426d42a24c76b
Hareesh suReddy <hareeshsureddy@gmail.com>
2026-01-20 23:25:34 +0530

    Easy 0 ms (100%) 11.6 MB (23.53%) 01-20-2026

---
+++ 3314-construct-the-minimum-bitwise-array-i/3314-construct-the-minimum-bitwise-array-i.c
@@ -0,0 +1,18 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* minBitwiseArray(int* nums, int numsSize, int* returnSize) {
+    int *res=(int*)calloc(numsSize,sizeof(int));
+    *returnSize=numsSize;
+    for(int i=0;i<numsSize;i++){
+        int num=nums[i];
+        for(int j=0;j<num;j++){
+            if((j|(j+1))==num){
+                res[i]=j;
+                break;
+            }
+        }
+        if(!res[i]) res[i]=-1;
+    }
+    return res;
+}
