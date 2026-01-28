9399b253753bdb47e1f41342904ccd91bdc82fed
Hareesh suReddy <hareeshsureddy@gmail.com>
2026-01-28 23:50:48 +0530

    Easy 0 ms (100%) 8 MB (8.33%) 01-28-2026

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.c
@@ -0,0 +1,17 @@
+bool canBeEqual(char* s1, char* s2) {
+    if(s1[0]==s2[0] || s1[0]==s2[2])
+        if(s1[2]==s2[0] || s1[2]==s2[2])
+            if(s1[1]==s2[1] || s1[1]==s2[3])
+                if(s1[3]==s2[1] || s1[3]==s2[3])
+                return true;
+                else
+                return false;
+            else
+            return false;
+        else
+        return false;
+    else
+    return false;
+
+
+}
