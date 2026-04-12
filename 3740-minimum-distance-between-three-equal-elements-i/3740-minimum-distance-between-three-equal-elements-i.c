25a57bd021c6aa63669e0d73eae063f5a9a11892
Hareesh suReddy <hareeshsureddy@gmail.com>
2026-04-13 03:23:43 +0530

    Easy 0 ms (100%) 9.8 MB (89.99%) 04-13-2026

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.c
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.c
@@ -1,25 +1,25 @@
 int minimumDistance(int* nums, int numsSize) {
-    int minimumDistance=-1;
-    for(int i=0;i<numsSize;i++){
-        int j=i+1;
-        bool status=false;
-        for(;j<numsSize;j++){
-            if(nums[i]==nums[j]) break;
+    int minimumDistance = -1;
+    for (int i = 0; i < numsSize; i++) {
+        int j = i + 1;
+        bool status = false;
+        for (; j < numsSize; j++) {
+            if (nums[i] == nums[j])
+                break;
         }
-        int k=j+1;
-        for(;k<numsSize;k++){
-            if(nums[i]==nums[k]) {
-                status=true;
+        int k = j + 1;
+        for (; k < numsSize; k++) {
+            if (nums[i] == nums[k]) {
+                status = true;
                 break;
             }
         }
-        if(status){
-            int distance=abs(i - j) + abs(j - k) + abs(k - i);
-            if(minimumDistance==-1)
-            minimumDistance=distance;
-            if(distance<minimumDistance)
-            minimumDistance=distance;
-
+        if (status) {
+            int distance = abs(i - j) + abs(j - k) + abs(k - i);
+            if (minimumDistance == -1)
+                minimumDistance = distance;
+            if (distance < minimumDistance)
+                minimumDistance = distance;
         }
     }
     return minimumDistance;
