f32b4e73468f00241c85405c9ff51268df681527
Hareesh suReddy <hareeshsureddy@gmail.com>
2026-04-22 01:44:19 +0530

    Easy 0 ms (100%) 8.9 MB (80.33%) 04-22-2026

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.c
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.c
@@ -1,11 +1,29 @@
+/*
 int maxDistance(int* colors, int colorsSize) {
     int maxDistance=0;
     for (int i = 0; i < colorsSize; i++) {
-        for(int j=0;j<colorsSize;j++){
+        for(int j=colorsSize-1;j>i;j--){
             if(colors[i]!=colors[j]){
                  maxDistance=fmax( maxDistance,abs(i-j));
             }
         }
     }
     return maxDistance;
+}
+*/
+int maxDistance(int* colors, int colorsSize) {
+    int maxDistance1=0,maxDistance2=0;
+    for(int i=colorsSize-1;i>0;i--){
+        if(colors[0]!=colors[i]){
+            maxDistance1=i;
+            break;
+        }
+    }
+    for(int i=0;i<colorsSize;i++){
+        if(colors[colorsSize-1]!=colors[i]){
+            maxDistance2=colorsSize-i-1;
+            break;
+        }
+    }
+    return fmax(maxDistance1,maxDistance2);
 }
